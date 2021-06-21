293f53a5bd87027814b15ec06dc20d9b37e0dd9f
anthony <anthony@madhive.com>
2021-06-21 14:14:17 -0400

    Made asking for invalid logger panic

--- logging/factory.go
+++ logging/factory.go
@@ -6,11 +6,16 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/MadBase/MadNet/constants"
 	"github.com/sirupsen/logrus"
 )
 
-var loggers = map[string]*logrus.Logger{}
-var loggersMutex = &sync.Mutex{}
+type loggerDetails struct {
+	sync.Once
+	loggers map[string]*logrus.Logger
+}
+
+var loggers loggerDetails // map[string]*logrus.Logger{}
 
 //LogFormatter applies consistent formatting to every message
 type LogFormatter struct {
@@ -39,33 +44,40 @@ func (logWriter *LogWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+func (ld *loggerDetails) init() {
+	ld.Do(func() {
+		loggers.loggers = make(map[string]*logrus.Logger, len(constants.ValidLoggers))
+		for _, loggerName := range constants.ValidLoggers {
+			formatter := &LogFormatter{frm: func(entry *logrus.Entry) ([]byte, error) {
+				defaultFormat, e := (&logrus.TextFormatter{
+					PadLevelText:    true,
+					TimestampFormat: "1-2|15:04:05.000",
+					FullTimestamp:   true}).Format(entry)
+
+				label := fmt.Sprintf("%-10s ", loggerName)
+
+				line := bytes.Join([][]byte{
+					[]byte(label), defaultFormat},
+					[]byte(" "))
+
+				return line, e
+			}}
+
+			logger := logrus.New()
+			logger.SetFormatter(formatter)
+			logger.SetLevel(logrus.InfoLevel)
+
+			loggers.loggers[loggerName] = logger
+		}
+	})
+}
+
 //GetLogger either returns an existing logger for package specified or creates a new one
-func GetLogger(pkgName string) *logrus.Logger {
-	loggersMutex.Lock()
-	logger, exists := loggers[pkgName]
-	loggersMutex.Unlock()
-
+func GetLogger(name string) *logrus.Logger {
+	loggers.init()
+	logger, exists := loggers.loggers[name]
 	if !exists {
-		formatter := &LogFormatter{frm: func(entry *logrus.Entry) ([]byte, error) {
-			defaultFormat, e := (&logrus.TextFormatter{PadLevelText: true, TimestampFormat: "1-2|15:04:05.000", FullTimestamp: true}).Format(entry)
-
-			pkg := fmt.Sprintf("%-10s ", pkgName)
-
-			line := bytes.Join([][]byte{
-				[]byte(pkg), defaultFormat},
-				[]byte(" "))
-
-			return line, e
-		}}
-
-		logger = logrus.New()
-		logger.SetFormatter(formatter)
-
-		logger.SetLevel(logrus.DebugLevel)
-
-		loggersMutex.Lock()
-		loggers[pkgName] = logger
-		loggersMutex.Unlock()
+		panic(fmt.Sprintf("Invalid logger requested: %v", name))
 	}
 
 	return logger
@@ -80,8 +92,9 @@ func GetLogWriter(pkgName string, level logrus.Level) *LogWriter {
 
 // GetKnownLoggers returns all loggers currently configured
 func GetKnownLoggers() []*logrus.Logger {
+	loggers.init()
 	ret := make([]*logrus.Logger, 0)
-	for _, logger := range loggers {
+	for _, logger := range loggers.loggers {
 		ret = append(ret, logger)
 	}
 
--- logging/factory_test.go
+++ logging/factory_test.go
@@ -1,44 +1,74 @@
-package logging
+package logging_test
 
 import (
 	"fmt"
 	"testing"
 
+	"github.com/MadBase/MadNet/constants"
+	"github.com/MadBase/MadNet/logging"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetLoggerNew(t *testing.T) {
-	logNumber := len(GetKnownLoggers())
+func TestGetValidLoggers(t *testing.T) {
+	n := len(constants.ValidLoggers)
+	assert.True(t, n > 0)
 
-	_ = GetLogger("TestGetLoggerNew")
+	for idx := 0; idx < n; idx++ {
+		logger := logging.GetLogger(constants.ValidLoggers[idx])
+		assert.NotNil(t, logger)
+	}
 
-	assert.Equal(t, logNumber+1, len(GetKnownLoggers()), "There should be 1 more logger.")
+}
+
+func TestGetInvalidLogger(t *testing.T) {
+	defer func() {
+		r := recover()
+		assert.NotNil(t, r, "Should have panic'ed when requesting a bad logger")
+	}()
+	logger := logging.GetLogger("Foo")
+	logger.Infof("should never happen")
 }
 
 func TestGetLoggerDefaultLevel(t *testing.T) {
-	logger := GetLogger("TestGetLoggerDefaultLevel")
-	assert.Equal(t, logrus.DebugLevel, logger.GetLevel(), "Default level is DEBUG")
+	loggerName := constants.ValidLoggers[0]
+	logger := logging.GetLogger(loggerName)
+	assert.Equal(t, logrus.InfoLevel, logger.GetLevel(), "Default level is INFO")
+}
+
+func TestLog(t *testing.T) {
+	defer func() {
+		r := recover()
+		assert.Nil(t, r, "No panics during/after logging")
+	}()
+	logger := logging.GetLogger(constants.ValidLoggers[0])
+	logger.Infof("foo")
 }
 
 func TestGetLoggerPreserveLevel(t *testing.T) {
-	const loggerName = "TestGetLoggerPreserveLevel"
+	loggerName := constants.ValidLoggers[0]
 
-	logger := GetLogger(loggerName)
-	assert.Equal(t, logrus.DebugLevel, logger.GetLevel(), "Default level is DEBUG")
+	logger := logging.GetLogger(loggerName)
+	assert.Equal(t, logrus.InfoLevel, logger.GetLevel(), "Default should be INFO")
+
 	logger.SetLevel(logrus.DebugLevel)
 
-	newLogger := GetLogger(loggerName)
+	newLogger := logging.GetLogger(loggerName)
 	assert.Equal(t, logrus.DebugLevel, newLogger.GetLevel(), "Level was set to DEBUG")
 }
 
 func TestGetLogWriter(t *testing.T) {
-	const loggerName = "TestGetLogWriter"
-	logger := GetLogger(loggerName)
+	loggerName := constants.ValidLoggers[0]
+	logger := logging.GetLogger(loggerName)
 	logger.SetLevel(logrus.ErrorLevel)
 
-	out := GetLogWriter(loggerName, logrus.WarnLevel)
+	out := logging.GetLogWriter(loggerName, logrus.WarnLevel)
 
 	// TODO: Probably have to capture stdout to really automate this test
 	fmt.Fprintf(out, "Should not be visible even with a test -v ./...")
+}
+
+func TestGetKnownLoggers(t *testing.T) {
+	known := logging.GetKnownLoggers()
+	assert.Equal(t, len(constants.ValidLoggers), len(known))
 }
