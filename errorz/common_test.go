2cc9dbec2b33bc3c9eae468c6b78c4cc12d588d8
kasvtv <kazaakas@hotmail.com>
2021-09-02 16:10:41 +0200

    add testing

--- errorz/common_test.go
+++ errorz/common_test.go
@@ -9,12 +9,30 @@ func TestErrInvalidWrapping(t *testing.T) {
 	err := NewErrConsensus("something low level went wrong", true)
 	err2 := WrapErrInvalid(err, "something high level went wrong")
 
+	if err2.Error() != "the object is invalid:something high level went wrong:\nsomething low level went wrong" {
+		t.Fatal("error message of wrapper error should be properly concatenated with wrapped error")
+	}
+
 	var wrapped *ErrConsensus
 	if !errors.As(err2, &wrapped) {
-		t.Fatal("err2 should contain err")
+		t.Fatal("err2 should unwrap to err using errors.As")
 	}
 
 	if !wrapped.isLocal {
-		t.Fatal("isLocal should be true")
+		t.Fatal("wrapped attributes should be readable")
 	}
+
+	if wrapped.Error() != "something low level went wrong" {
+		t.Fatal("wrapped Error() should return proper error string")
+	}
+
+}
+
+func TestErrInvalidUnwrapped(t *testing.T) {
+	err := ErrInvalid{}.New("something high level went wrong")
+
+	if err.Error() != "the object is invalid:something high level went wrong" {
+		t.Fatal("ErrInvalid needs proper Error() implementation when unwrapped")
+	}
+
 }
