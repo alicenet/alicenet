c47040f6b9b766b2b2c6a3454bc23a679c656c7e
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-09-08 17:16:20 -0300

    Clean up tests

--- consensus/objs/ricleo_test.go
+++ consensus/objs/ricleo_test.go
@@ -204,6 +204,94 @@ func makeTransfer(t *testing.T, sender objs.Signer, receiver objs.Signer, transf
 	return tx
 }
 
+func GenerateBlock(chain []*BClaims, stateRoot []byte, txHshLst [][]byte) ([]*BClaims, error) {
+	var prevBlock []byte
+	var headerRoot []byte
+	if len(chain) == 0 {
+		chain = []*BClaims{}
+		prevBlock = crypto.Hasher([]byte("foo"))
+		headerRoot = crypto.Hasher([]byte(""))
+	} else {
+		_prevBlock, err := chain[len(chain)-1].BlockHash()
+		if err != nil {
+			return nil, err
+		}
+		prevBlock = _prevBlock
+		headerRoot = crypto.Hasher([]byte("")) // todo: how to generate the block smt
+	}
+	txRoot, err := MakeTxRoot(txHshLst) // generating the smt root
+	log.Printf("txRoot height: (%d): %x\n", len(chain)+1, txRoot)
+	if err != nil {
+		if err != nil {
+			return nil, err
+		}
+	}
+	bclaims := &BClaims{
+		ChainID:    1,
+		Height:     uint32(len(chain) + 1),
+		TxCount:    uint32(len(txHshLst)),
+		PrevBlock:  prevBlock,
+		TxRoot:     txRoot,
+		StateRoot:  stateRoot,
+		HeaderRoot: headerRoot,
+	}
+	chain = append(chain, bclaims)
+
+	log.Printf(
+		"\nBlock: {\n\tChainID: %d\n\tHeight: %d\n\tTxCount: %d\n\tPrevBlock: %x\n\tTxRoot: %x\n\tStateRoot: %x\n\tHeaderRoot: %x\n}\n\n",
+		bclaims.ChainID,
+		bclaims.Height,
+		bclaims.TxCount,
+		bclaims.PrevBlock,
+		bclaims.TxRoot,
+		bclaims.StateRoot,
+		bclaims.HeaderRoot,
+	)
+	return chain, nil
+}
+
+func getAllStateMerkleProofs(hndlr *utxohandler.UTXOHandler, txs []*objs.Tx) func(txn *badger.Txn) error {
+	fn := func(txn *badger.Txn) error {
+		stateTrie, err := hndlr.GetTrie().GetCurrentTrie(txn)
+		if err != nil {
+			return err
+		}
+		log.Printf("Trie height: %d\n", stateTrie.TrieHeight)
+		for _, tx := range txs {
+			txHash, err := tx.TxHash()
+			if err != nil {
+				return err
+			}
+			log.Println("===========Proof of inclusion=========")
+			log.Printf("Tx: %x\n", txHash)
+			log.Println("======================================")
+			utxoIDs, err := tx.GeneratedUTXOID()
+			if err != nil {
+				return err
+			}
+			for i, utxoID := range utxoIDs {
+				auditPath, keyIncluded, key, value, err := stateTrie.MerkleProof(txn, utxoID) // *badger.Txn, key []byte
+				if err != nil {
+					return err
+				}
+				log.Printf("UTXOID: %x\n", utxoID)
+				log.Printf("auditPath: %x\n", auditPath)
+				log.Print("Included:", keyIncluded)
+				log.Printf("Proof key: %x\n", key)
+				log.Printf("Proof value: %x\n", value)
+				if len(utxoIDs) > i+1 {
+					log.Println("---------------------")
+				}
+			}
+			log.Println("======================================")
+			log.Println()
+
+		}
+		return nil
+	}
+	return fn
+}
+
 func TestRicLeo(t *testing.T) {
 	// Database setup
 	log.Println("TestRicLeo starting")
@@ -247,43 +335,31 @@ func TestRicLeo(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	///////// Block 1 ////////////
+	log.Println("Block 1:")
 	// Creating First UTXO
-	// signer 1
-	value := &uint256.Uint256{}
-	_, _ = value.FromUint64(10)
-	d := makeDeposit(t, signer, 1, 1, value) // created pre-image object
-	utxoDep := &objs.TXOut{}
-	err = utxoDep.NewValueStore(d) // <- create a txout to be used on Vout (a.k.a UTXO) from pre-image object
-	if err != nil {
-		t.Fatal(err)
-	}
-	tx := makeTxs(t, signer, d)
-	txHash, err := tx.TxHash()
-	if err != nil {
-		t.Fatal(err)
-	}
-	log.Printf("Tx hash: %x", txHash)
-
-	// Create the transaction with that first Utxo (coinbase generation)
-	// Create the block
-	err = db.Update(func(txn *badger.Txn) error {
-		_, err := hndlr.IsValid(txn, []*objs.Tx{tx}, 1, objs.Vout{utxoDep})
+	var txs []*objs.Tx
+	var deposits []*objs.ValueStore
+	var txHshLst [][]byte
+	for i := uint64(0); i < 5; i++ {
+		value := &uint256.Uint256{}
+		_, _ = value.FromUint64(i + 1)
+		deposits = append(deposits, makeDeposit(t, signer, 1, int(i), value)) // created pre-image object
+		txs = append(txs, makeTxs(t, signer, deposits[i]))
+		txHash, err := txs[i].TxHash()
 		if err != nil {
 			t.Fatal(err)
 		}
-		return nil
-	})
-	if err != nil {
-		t.Fatal(err)
+		log.Printf("Tx hash (%d): %x", i, txHash)
+		txHshLst = append(txHshLst, txHash)
 	}
 
 	var stateRoot []byte
 	err = db.Update(func(txn *badger.Txn) error {
-		stateRoot, err = hndlr.ApplyState(txn, []*objs.Tx{tx}, 2)
+		stateRoot, err = hndlr.ApplyState(txn, txs, 1)
 		if err != nil {
-			t.Fatal(err) //     ricleo_test.go:216: the object is invalid:not initialized
+			t.Fatal(err)
 		}
-
 		log.Printf("stateRoot: %x\n", stateRoot)
 		return nil
 	})
@@ -291,138 +367,98 @@ func TestRicLeo(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	//var stateTrie *trie.SMT
-	err = db.Update(func(txn *badger.Txn) error {
-		stateTrie, err := hndlr.GetTrie().GetCurrentTrie(txn)
-		if err != nil {
-			t.Fatal(err) //     ricleo_test.go:216: the object is invalid:not initialized
-		}
-		utxoIDs, err := tx.GeneratedUTXOID()
-		log.Printf("Trie height: %d\n", stateTrie.Height())
-		auditPath, keyIncluded, key, value, err := stateTrie.MerkleProof(txn, utxoIDs[0]) // *badger.Txn, key []byte
-		if err != nil {
-			log.Fatal("error getting merkle proof", err)
-		}
-		log.Println("==============Proof of inclusion============")
-		log.Printf("auditPath: %x\n", auditPath)
-		log.Print("Included:", keyIncluded)
-		log.Printf("key: %x\n", key)
-		log.Printf("value: %x\n", value)
-		return nil
-	})
+	err = db.Update(getAllStateMerkleProofs(hndlr, txs))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// generating block 1
-	chain := []*BClaims{}
-	txHashes := [][][]byte{}
-	txHshLst := [][]byte{txHash}
-	txRoot, err := MakeTxRoot(txHshLst) // generating the smt root
-	log.Printf("txRoot (0): %x\n", txRoot)
+	// Generating block 1
+	chain, err := GenerateBlock(nil, stateRoot, txHshLst)
 	if err != nil {
-		log.Fatal("error making txroot")
+		t.Fatal(err)
 	}
-	txHashes = append(txHashes, txHshLst)
-	bclaims := &BClaims{
-		ChainID:    1,
-		Height:     1,
-		TxCount:    1,
-		PrevBlock:  crypto.Hasher([]byte("foo")),
-		TxRoot:     txRoot,
-		StateRoot:  stateRoot,
-		HeaderRoot: crypto.Hasher([]byte("")), //todo: how to generate the block smt
-	}
-	chain = append(chain, bclaims)
-
-	log.Printf("Block: {\n\tChainID: %d\n\tHeight: %d\n\tTxCount: %d\n\tPrevBlock: %x\n\tTxRoot: %x\n\tStateRoot: %x\n\tHeaderRoot: %x\n}\n", bclaims.ChainID, bclaims.Height, bclaims.TxCount, bclaims.PrevBlock, bclaims.TxRoot, bclaims.StateRoot, bclaims.HeaderRoot)
 
 	////////// Block 2 /////////////
 	// this is consuming utxo generated on block 1
-	//tx := makeTxs(t, signer, d) // <- creating a txin that consumes the UTXO created on block 1
-
-	// signer 2
-	//d2 := makeDeposit(t, signer2, 1, 1, uint256.Two())
-	//utxoDep2 := &objs.TXOut{}
-	// err = utxoDep.NewValueStore(d2)
-	// if err != nil {,
-	// 	t.Fatal(err)
-	// }
-	tx2 := makeTransfer(t, signer, signer2, 2, d)
+	log.Println("Block 2:")
+	tx2 := makeTransfer(t, signer, signer2, 1, deposits[1])
 	txHash2, err := tx2.TxHash()
 	if err != nil {
 		t.Fatal(err)
 	}
 	log.Printf("Tx hash: %x", txHash2)
-	// err = db.Update(func(txn *badger.Txn) error {
-	// 	_, err := hndlr.IsValid(txn, []*objs.Tx{tx2}, 1, objs.Vout{utxoDep2})
-	// 	if err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// 	return nil
-	// })
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
 	err = db.Update(func(txn *badger.Txn) error {
 		stateRoot, err = hndlr.ApplyState(txn, []*objs.Tx{tx2}, 2)
 		if err != nil {
-			t.Fatal(err) //     ricleo_test.go:216: the object is invalid:not initialized
+			t.Fatal(err)
 		}
-
 		log.Printf("stateRoot2: %x\n", stateRoot)
 		return nil
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
+	err = db.Update(getAllStateMerkleProofs(hndlr, []*objs.Tx{tx2}))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// generating block2
-	bhsh, err := chain[len(chain)-1].BlockHash()
+	chain, err = GenerateBlock(chain, stateRoot, [][]byte{txHash2})
 	if err != nil {
 		t.Fatal(err)
 	}
-	txHshLst = [][]byte{txHash2}
-	txRoot, err = MakeTxRoot(txHshLst)
-	log.Printf("txRoot (1): %x\n", txRoot)
-	if err != nil {
-		t.Fatal(err)
-	}
-	txHashes = append(txHashes, txHshLst)
-	bclaims = &BClaims{
-		ChainID:    1,
-		Height:     uint32(len(chain) + 1),
-		TxCount:    1,
-		PrevBlock:  bhsh,
-		TxRoot:     txRoot,
-		StateRoot:  stateRoot,
-		HeaderRoot: chain[len(chain)-1].HeaderRoot,
-	}
-	chain = append(chain, bclaims)
-	log.Printf("Block: {\n\tChainID: %d\n\tHeight: %d\n\tTxCount: %d\n\tPrevBlock: %x\n\tTxRoot: %x\n\tStateRoot: %x\n\tHeaderRoot: %x\n}\n", bclaims.ChainID, bclaims.Height, bclaims.TxCount, bclaims.PrevBlock, bclaims.TxRoot, bclaims.StateRoot, bclaims.HeaderRoot)
 
-	utxoIDs, err := tx.GeneratedUTXOID()
+	//////////// Generating BClaims and PClaims ////////////////////
+	bnVal := &crypto.BNGroupValidator{}
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = db.Update(func(txn *badger.Txn) error {
-		_, missing, err := hndlr.Get(txn, utxoIDs)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if len(missing) != 0 {
-			t.Fatal("missing utxoID")
-		}
-		_, missing, err = hndlr.Get(txn, [][]byte{crypto.Hasher([]byte("nil"))})
-		if err != nil {
-			t.Fatal(err)
-		}
-		if len(missing) != 1 {
-			t.Fatal("not missing utxoID")
-		}
-		return nil
-	})
+	bclaims := chain[0]
+	bhsh, err := bclaims.BlockHash()
 	if err != nil {
 		t.Fatal(err)
 	}
+	gk := &crypto.BNGroupSigner{}
+	gk.SetPrivk(crypto.Hasher([]byte("secret")))
+	sig, err := gk.Sign(bhsh)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bh := &BlockHeader{
+		BClaims:  bclaims,
+		SigGroup: sig,
+		TxHshLst: txHshLst,
+	}
+	err = bh.ValidateSignatures(bnVal)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rcert, err := bh.GetRCert()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = rcert.ValidateSignature(bnVal)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bclaimsBin, err := chain[0].MarshalBinary()
+	log.Printf("BClaim block 1: %x", bclaimsBin)
+	log.Printf("SigGrup Block 1: %x", rcert.SigGroup)
+
+	pclms := &PClaims{
+		BClaims: chain[1],
+		RCert:   rcert,
+	}
+
+	pClaimsBin, err := pclms.MarshalBinary()
+	log.Printf("PClaims Block 2: %x", pClaimsBin)
+	prop := &Proposal{
+		PClaims:  pclms,
+		TxHshLst: [][]byte{txHash2},
+	}
+	err = prop.Sign(signer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("Sig PClaims Block 2: %x", prop.Signature)
 }
