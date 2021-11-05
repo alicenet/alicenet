8e77479aa77940fa085254bcedbb9a0d428ab4b0
Chris Gorman <chgorman@users.noreply.github.com>
2021-11-05 13:44:24 -0400

    Added nonzero fees for objects.

---
+++ dynamics/application.go
@@ -0,0 +1,21 @@
+package dynamics
+
+import "math/big"
+
+func bigFromBase10(s string) *big.Int {
+	n, _ := new(big.Int).SetString(s, 10)
+	return n
+}
+
+// atomicSwapFee is the initial fee for the AtomicSwap object.
+var atomicSwapFee = bigFromBase10("1000000")
+
+// dataStoreEpochFee is the initial fee for the DataStore object;
+// this fee is the fee per epoch.
+var dataStoreEpochFee = bigFromBase10("1000000")
+
+// valueStoreFee is the initial fee for the ValueStore object.
+var valueStoreFee = bigFromBase10("1000000")
+
+// minTxFee is the initial minimum transaction fee for a Tx object.
+var minTxFee = bigFromBase10("1000000")
