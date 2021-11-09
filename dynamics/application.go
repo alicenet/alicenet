ca44a6f67f1643c9531a954843269baa0cf3a692
Chris Gorman <chgorman@users.noreply.github.com>
2021-11-09 10:30:48 -0500

    Updated fees.

--- dynamics/application.go
+++ dynamics/application.go
@@ -8,14 +8,14 @@ func bigFromBase10(s string) *big.Int {
 }
 
 // atomicSwapFee is the initial fee for the AtomicSwap object.
-var atomicSwapFee = bigFromBase10("1")
+var atomicSwapFee = bigFromBase10("2")
 
 // dataStoreEpochFee is the initial fee for the DataStore object;
 // this fee is the fee per epoch.
-var dataStoreEpochFee = bigFromBase10("2")
+var dataStoreEpochFee = bigFromBase10("3")
 
 // valueStoreFee is the initial fee for the ValueStore object.
-var valueStoreFee = bigFromBase10("3")
+var valueStoreFee = bigFromBase10("1")
 
 // minTxFee is the initial minimum transaction fee for a Tx object.
 var minTxFee = bigFromBase10("4")
