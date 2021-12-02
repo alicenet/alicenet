66a4506d5958b42965a6ce82d2a945ff6eb2500c
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-12-02 16:49:24 -0300

    Revert validator 0 bash script changes

--- scripts/validator0.sh
+++ scripts/validator0.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-timestamp=$(date "+%Y%m%d-%H:%M")
-./madnet --config ./assets/config/validator0.toml validator 2>&1 | tee "validator0.$timestamp.log"
+./madnet --config ./assets/config/validator0.toml validator
