1d5f6ab767895ccd0ec3b13ab6c4a9baa4ba8533
Keiron <keiron.gulrajani@gmail.com>
2022-03-14 14:16:31 +0100

    Fixed issue introduced by static analysis fix Added test in Validator NFT business logic to cover svg generation in token URI

--- bridge/test/stakeNFTPositionDescriptor/stake-nft-metadata.test.ts
+++ bridge/test/stakeNFTPositionDescriptor/stake-nft-metadata.test.ts
@@ -13,7 +13,7 @@ describe("StakeNFTPositionDescriptor: Tests StakeNFTPositionDescriptor methods",
   let fixture: Fixture
   let adminSigner: Signer
   let stakeNFT: StakeNFT
-
+  let stakeNFTPositionDescriptor: StakeNFTPositionDescriptor
   let stakeAmount = 20000
   let stakeAmountMadWei = ethers.utils.parseUnits(stakeAmount.toString(), 18)
   let lockTime = 1
@@ -26,6 +26,7 @@ describe("StakeNFTPositionDescriptor: Tests StakeNFTPositionDescriptor methods",
     adminSigner = await getValidatorEthAccount(admin.address)
 
     stakeNFT = fixture.stakeNFT
+    stakeNFTPositionDescriptor = fixture.stakeNFTPositionDescriptor
 
     await fixture.madToken.approve(
       fixture.stakeNFT.address,
@@ -45,42 +46,66 @@ describe("StakeNFTPositionDescriptor: Tests StakeNFTPositionDescriptor methods",
     )
   })
 
-  it("Should return correct token uri", async function () {
-    const positionData = await stakeNFT.getPosition(tokenId)
+  describe("Given valid token id", async () => {
+    let positionData
+    let svg: string
+    let tokenUriJson: string
+    let expectedTokenUriData: string
+    beforeEach(async function () {
+      positionData = await stakeNFT.getPosition(tokenId)
 
-    const svg =
-      `<svg width="500" height="500" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink='http://www.w3.org/1999/xlink'>` +
-      `<text x='10' y='20'>Shares: ${positionData.shares.toString()}</text>` +
-      `<text x='10' y='40'>Free after: ${positionData.freeAfter.toString()}</text>` +
-      `<text x='10' y='60'>Withdraw Free After: ${positionData.withdrawFreeAfter.toString()}</text>` +
-      `<text x='10' y='80'>Accumulator (ETH): ${positionData.accumulatorEth.toString()}</text>` +
-      `<text x='10' y='100'>Accumulator (Token): ${positionData.accumulatorToken.toString()}</text></svg>`
+      svg =
+        `<svg width="500" height="500" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink='http://www.w3.org/1999/xlink'>` +
+        `<text x='10' y='20'>Shares: ${positionData.shares.toString()}</text>` +
+        `<text x='10' y='40'>Free after: ${positionData.freeAfter.toString()}</text>` +
+        `<text x='10' y='60'>Withdraw Free After: ${positionData.withdrawFreeAfter.toString()}</text>` +
+        `<text x='10' y='80'>Accumulator (ETH): ${positionData.accumulatorEth.toString()}</text>` +
+        `<text x='10' y='100'>Accumulator (Token): ${positionData.accumulatorToken.toString()}</text></svg>`
 
-    const tokenUriJson =
-      `{"name":"MadNET Staked token for position #1", ` +
-      `"description":"This NFT represents a staked position on MadNET.` +
-      `\\nThe owner of this NFT can modify or redeem the position.` +
-      `\\n Shares: ${positionData.shares.toString()}` +
-      `\\nFree After: ${positionData.freeAfter.toString()}` +
-      `\\nWithdraw Free After: ${positionData.withdrawFreeAfter.toString()}` +
-      `\\nAccumulator Eth: ${positionData.accumulatorEth.toString()}` +
-      `\\nAccumulator Token: ${positionData.accumulatorToken.toString()}` +
-      `\\nToken ID: ${tokenId.toString()}", ` +
-      `"image": "data:image/svg+xml;base64,${btoa(svg)}"}`
+      tokenUriJson =
+        `{"name":"MadNET Staked token for position #1", ` +
+        `"description":"This NFT represents a staked position on MadNET.` +
+        `\\nThe owner of this NFT can modify or redeem the position.` +
+        `\\n Shares: ${positionData.shares.toString()}` +
+        `\\nFree After: ${positionData.freeAfter.toString()}` +
+        `\\nWithdraw Free After: ${positionData.withdrawFreeAfter.toString()}` +
+        `\\nAccumulator Eth: ${positionData.accumulatorEth.toString()}` +
+        `\\nAccumulator Token: ${positionData.accumulatorToken.toString()}` +
+        `\\nToken ID: ${tokenId.toString()}", ` +
+        `"image": "data:image/svg+xml;base64,${btoa(svg)}"}`
 
-    const expectedTokenUriData = `data:application/json;base64,${btoa(
-      tokenUriJson
-    )}`
+      expectedTokenUriData = `data:application/json;base64,${btoa(
+        tokenUriJson
+      )}`
+    })
 
-    const tokenUri = await stakeNFT.tokenURI(tokenId)
+    it("StakeNFTPositionDescriptor should return correct token uri", async function () {
+      const tokenUri = await stakeNFTPositionDescriptor.tokenURI(
+        stakeNFT.address,
+        tokenId
+      )
 
-    const parsedJson = JSON.parse(
-      atob(tokenUri.replace("data:application/json;base64,", ""))
-    )
+      const parsedJson = JSON.parse(
+        atob(tokenUri.replace("data:application/json;base64,", ""))
+      )
 
-    await expect(tokenUri).to.be.equal(expectedTokenUriData)
-    await expect(
-      atob(parsedJson.image.replace("data:image/svg+xml;base64,", ""))
-    ).to.be.equal(svg)
+      await expect(tokenUri).to.be.equal(expectedTokenUriData)
+      await expect(
+        atob(parsedJson.image.replace("data:image/svg+xml;base64,", ""))
+      ).to.be.equal(svg)
+    })
+
+    it("StakeNFT contract Should return correct token uri", async function () {
+      const tokenUri = await stakeNFT.tokenURI(tokenId)
+
+      const parsedJson = JSON.parse(
+        atob(tokenUri.replace("data:application/json;base64,", ""))
+      )
+
+      await expect(tokenUri).to.be.equal(expectedTokenUriData)
+      await expect(
+        atob(parsedJson.image.replace("data:image/svg+xml;base64,", ""))
+      ).to.be.equal(svg)
+    })
   })
 })
