92dedb7bc884b61f8a6f6a9a9eb1c9a4613eb3fc
Ricardo Pinto <ricardo.soares.pinto@gmail.com>
2022-06-08 13:38:50 +0000

    Polling working with last 2 epochs of data

--- consensus/accusation/manager.go
+++ consensus/accusation/manager.go
@@ -55,22 +55,72 @@ func (m *Manager) Stop() {
 }
 
 func (m *Manager) run() {
+	// TODO: should only check roundStates from the last 2 epochs, including the current one
 	// todo: load current polling height and round from DB, to resume operations in case of a node restart
 	// var height uint32 = 4895 // bugs at 4899 (round 1-4 key not found), 4903 (round 2 was printed in status but it doesnt actually exist), 4929 (round 1-4 key not found)
-	var height uint32 = 5490 // 5502
-	var round uint32 = 1
-	var hasRoundStatesAtHeight bool = false
 
-	// poll validators' roundStates
+	var height uint32 = 2
+	var round uint32 = 1
+
+	// first let's wait for synchronization
 	for {
 		time.Sleep(1000 * time.Millisecond)
 
 		// only poll data if the node is synchronized
 		// todo: can we get this without requiring the adminHandler here? maybe a remoteVar()?
 		if !m.adminHandler.IsSynchronized() {
-			m.logger.Infof("AccusationManager: admin.Handler is not synchronized, skipping round state polling")
+			m.logger.Debugf("AccusationManager: admin.Handler is not synchronized, skipping round state polling")
 			continue
 		}
+
+		// make sure we only check the last 2 epochs
+		currentHeight := uint32(0)
+		err := m.database.View(func(txn *badger.Txn) error {
+			// get the latest block height
+			hdr, err := m.database.GetBroadcastBlockHeader(txn)
+			if err != nil {
+				m.logger.Warnf("AccusationManager: could not get current height: %v", err)
+				return err
+			}
+			currentHeight = hdr.BClaims.Height
+			m.logger.Debugf("AccusationManager: got initial height: %d", currentHeight)
+
+			return nil
+		})
+
+		if err != nil {
+			if errors.Is(err, badger.ErrKeyNotFound) {
+				continue
+			}
+			panic(fmt.Sprintf("AccusationManager: could not get current height: %v", err))
+		}
+
+		// calculate which height to start polling from, only considering the last 2 epochs
+		currentEpoch := currentHeight / constants.EpochLength
+		m.logger.Debugf("AccusationManager: got initial currentEpoch: %d", currentEpoch)
+		startAtEpoch := currentEpoch
+		if startAtEpoch >= 2 {
+			startAtEpoch = startAtEpoch - 2
+		}
+		m.logger.Debugf("AccusationManager: got initial startAtEpoch: %d", startAtEpoch)
+		startAtHeight := startAtEpoch * constants.EpochLength
+		m.logger.Debugf("AccusationManager: got initial startAtHeight: %d", startAtHeight)
+		height = startAtHeight
+
+		if height <= 1 {
+			height = 2
+		}
+
+		m.logger.Infof("AccusationManager: polling validators' roundStates from height %d", height)
+
+		break
+	}
+
+	var hasRoundStatesAtHeight bool = false
+
+	// now we poll validators' roundStates
+	for {
+		time.Sleep(100 * time.Millisecond)
 
 		// fetch round states from DB
 		rss, err := m.fetchRoundStates(height, round)
@@ -99,14 +149,12 @@ func (m *Manager) run() {
 		// todo: save current polling height and round into DB, to resume operations in case of a node restart
 
 		// compute next height and round
-		// todo: find a way to get the max rounds possible, instead of hardcoding it as 4
 		if round >= constants.DEADBLOCKROUND {
 			// panic if we did not find any round states at this height
 			if !hasRoundStatesAtHeight {
 				m.logger.WithFields(logrus.Fields{
 					"height": height,
-					"round":  round,
-				}).Panicf("AccusationManager: no roundStates at height %d", height)
+				}).Panic("AccusationManager: no roundStates at height %d", height)
 			}
 
 			round = 1
@@ -132,6 +180,7 @@ func (m *Manager) fetchRoundStates(height uint32, round uint32) ([]*objs.RoundSt
 		currentHeight := hdr.BClaims.Height
 		//m.logger.Infof("AccusationManager: currentHeight %d", currentHeight)
 
+		// don't poll roundStates from heights in the future, because they don't exist yet
 		if height > currentHeight {
 			return ErrOverCurrentHeight
 		}
@@ -139,11 +188,10 @@ func (m *Manager) fetchRoundStates(height uint32, round uint32) ([]*objs.RoundSt
 		// get the validatorSet at a certain height
 		vs, err := m.database.GetValidatorSet(txn, height)
 		if err != nil {
-			m.logger.Errorf("AccusationManager: could not fetch validator set: %v", err)
 			if errors.Is(err, badger.ErrKeyNotFound) {
 				return nil
 			}
-			//m.logger.Errorf("AccusationManager: could not fetch validator set: %t %v", err, err)
+			m.logger.Errorf("AccusationManager: could not fetch validator set: %t %v", err, err)
 			return err
 		}
 
