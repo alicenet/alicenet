ricardopinto/MadNet

Accusation manager working with workers

consensus/synchronizer.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/MadBase/MadNet/application"
+	"github.com/MadBase/MadNet/consensus/accusation"
 	"github.com/MadBase/MadNet/consensus/admin"
 	"github.com/MadBase/MadNet/consensus/db"
 	"github.com/MadBase/MadNet/consensus/evidence"
@@ -207,6 +208,7 @@ type Synchronizer struct {
 	appHandler      *application.Application
 	adminHandler    *admin.Handlers
 	peerMan         *peering.PeerManager
+	accusationMan   *accusation.Manager
 
 	initialized   *setOnceVar
 	peerMinThresh *remoteVar
@@ -217,7 +219,7 @@ type Synchronizer struct {
 }
 
 // Init initializes the struct
-func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc *gossip.Client, gh *gossip.Handlers, ep *evidence.Pool, eng *lstate.Engine, app *application.Application, ah *admin.Handlers, pman *peering.PeerManager, storage dynamics.StorageGetter) {
+func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc *gossip.Client, gh *gossip.Handlers, ep *evidence.Pool, eng *lstate.Engine, app *application.Application, ah *admin.Handlers, pman *peering.PeerManager, aman *accusation.Manager, storage dynamics.StorageGetter) {
 	s.logger = logging.GetLogger(constants.LoggerConsensus)
 	s.cdb = cdb
 	s.mdb = mdb
@@ -229,6 +231,7 @@ func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc
 	s.appHandler = app
 	s.adminHandler = ah
 	s.peerMan = pman
+	s.accusationMan = aman
 	s.wg = sync.WaitGroup{}
 	s.closeChan = make(chan struct{})
 	s.closeOnce = sync.Once{}
@@ -518,4 +521,21 @@ func (s *Synchronizer) setupLoops() {
 		s.wg.Add(1)
 		go s.loop(tdbgcLoopConfig)
 	}
+
+	accusationManagerLoopConfig := newLoopConfig().
+		withName("AccusationManagerLoop").
+		//withInitialDelay(9*constants.MsgTimeout).
+		withFn(s.accusationMan.Poll).
+		withFreq(100 * time.Millisecond).
+		withDelayOnConditionFailure(100 * time.Millisecond).
+		withLockFreeCondition(s.isNotClosing).
+		withLockFreeCondition(s.initialized.isSet).
+		withLockFreeCondition(s.ethSyncDone.isSet).
+		withLockFreeCondition(s.madSyncDone.isSet).
+		withLockFreeCondition(s.peerMinThresh.isSet).
+		withLock().
+		withLockedCondition(s.isNotClosing).
+		withLockedCondition(s.madSyncDone.isSet)
+	s.wg.Add(1)
+	go s.loop(accusationManagerLoopConfig)
 }

alicenet/alicenet

consensus/synchronizer.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/alicenet/alicenet/application"
+	"github.com/alicenet/alicenet/consensus/accusation"
 	"github.com/alicenet/alicenet/consensus/admin"
 	"github.com/alicenet/alicenet/consensus/db"
 	"github.com/alicenet/alicenet/consensus/evidence"
@@ -207,6 +208,7 @@ type Synchronizer struct {
 	appHandler      *application.Application
 	adminHandler    *admin.Handlers
 	peerMan         *peering.PeerManager
+	accusationMan   *accusation.Manager
 
 	initialized   *setOnceVar
 	peerMinThresh *remoteVar
@@ -217,7 +219,7 @@ type Synchronizer struct {
 }
 
 // Init initializes the struct
-func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc *gossip.Client, gh *gossip.Handlers, ep *evidence.Pool, eng *lstate.Engine, app *application.Application, ah *admin.Handlers, pman *peering.PeerManager, storage dynamics.StorageGetter) {
+func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc *gossip.Client, gh *gossip.Handlers, ep *evidence.Pool, eng *lstate.Engine, app *application.Application, ah *admin.Handlers, pman *peering.PeerManager, aman *accusation.Manager, storage dynamics.StorageGetter) {
 	s.logger = logging.GetLogger(constants.LoggerConsensus)
 	s.cdb = cdb
 	s.mdb = mdb
@@ -229,6 +231,7 @@ func (s *Synchronizer) Init(cdb *db.Database, mdb *badger.DB, tdb *badger.DB, gc
 	s.appHandler = app
 	s.adminHandler = ah
 	s.peerMan = pman
+	s.accusationMan = aman
 	s.wg = sync.WaitGroup{}
 	s.closeChan = make(chan struct{})
 	s.closeOnce = sync.Once{}
@@ -518,4 +521,21 @@ func (s *Synchronizer) setupLoops() {
 		s.wg.Add(1)
 		go s.loop(tdbgcLoopConfig)
 	}
+
+	accusationManagerLoopConfig := newLoopConfig().
+		withName("AccusationManagerLoop").
+		//withInitialDelay(9*constants.MsgTimeout).
+		withFn(s.accusationMan.Poll).
+		withFreq(100 * time.Millisecond).
+		withDelayOnConditionFailure(100 * time.Millisecond).
+		withLockFreeCondition(s.isNotClosing).
+		withLockFreeCondition(s.initialized.isSet).
+		withLockFreeCondition(s.ethSyncDone.isSet).
+		withLockFreeCondition(s.madSyncDone.isSet).
+		withLockFreeCondition(s.peerMinThresh.isSet).
+		withLock().
+		withLockedCondition(s.isNotClosing).
+		withLockedCondition(s.madSyncDone.isSet)
+	s.wg.Add(1)
+	go s.loop(accusationManagerLoopConfig)
 }