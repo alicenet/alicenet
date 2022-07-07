1ebfd56326a822066bc5152f5b8ae0265d36e33e
Ricardo Pinto <ricardo.soares.pinto@gmail.com>
2022-07-07 18:29:43 +0000

    progress in accusation of multiple proposal

--- consensus/accusation/detect_multiple_proposal.go
+++ consensus/accusation/detect_multiple_proposal.go
@@ -4,7 +4,15 @@ import "github.com/alicenet/alicenet/consensus/objs"
 
 func detectMultipleProposal(rs *objs.RoundState) (objs.Accusation, bool) {
 	// rs.Proposal and rs.ConflictingProposal should both not be nil
-	// proposals must have same height and round: PLcainm -> rcert -> rclaims (better to check the whole rclaims equality) (also check all fields inside rclaims for validity - especially PrevBlock)
+	if rs.Proposal == nil || rs.ConflictingProposal == nil {
+		return nil, false
+	}
+
+	// proposals must have same height and round: PClaim -> rcert -> rclaims (better to check the whole rclaims equality) (also check all fields inside rclaims for validity - especially PrevBlock)
+	if rs.Proposal.PClaims.RCert.RClaims.Height != rs.ConflictingProposal.PClaims.BClaims.Height || rs.Proposal.PClaims.RCert.RClaims.Round != rs.ConflictingProposal.PClaims.RCert.RClaims.Round {
+		return nil, false
+	}
+
 	// proposals must be valid prop.ValidateSignatures(crypto.Secp256k1Validator{}, crypto.BNGroupValidator{})
 	// signatures are different it means double spend
 
