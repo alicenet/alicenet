ed40bd9580953c74f1e19dd6fef817c6c0aca85a
Keiron <keiron.gulrajani@gmail.com>
2022-07-11 12:14:37 +0200

    More error codes

---
+++ bridge/contracts/libraries/errors/CryptoLibraryErrors.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT-open-group
+pragma solidity ^0.8.11;
+
+library CryptoLibraryErrors {
+    // CryptoLibrary errors
+    error EllipticCurveAdditionFailed();
+    error HashPointNotOnCurve();
+    error HashPointUnsafeForSigning();
+    error PointNotOnCurve();
+    error SignatureIndicesLengthMismatch(uint256 signaturesLength, uint256 indicesLength);
+    error SignaturesLengthThresholdNotMet(uint256 signaturesLength, uint256 threshold);
+    error InverseArrayIncorrect(uint256 signaturesLength, uint256 threshold);
+}
