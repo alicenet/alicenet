bcea7de5492ccff9b078a2249f0902d3028742ca
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-07-11 10:23:06 -0300

    Add dkg tasks back to fix git path reference

---
+++ layer1/dkg/dkgtasks/share_distribution_task.go
@@ -0,0 +1,192 @@
+package dkgtasks
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/alicenet/alicenet/layer1/dkg"
+	"github.com/alicenet/alicenet/layer1/dkg/math"
+	"github.com/alicenet/alicenet/layer1/interfaces"
+	"github.com/alicenet/alicenet/layer1/objects"
+	"github.com/sirupsen/logrus"
+)
+
+// ShareDistributionTask stores the data required safely distribute shares
+type ShareDistributionTask struct {
+	*ExecutionData
+}
+
+// asserting that ShareDistributionTask struct implements interface interfaces.Task
+var _ interfaces.Task = &ShareDistributionTask{}
+
+// NewShareDistributionTask creates a new task
+func NewShareDistributionTask(state *objects.DkgState, start uint64, end uint64) *ShareDistributionTask {
+	return &ShareDistributionTask{
+		ExecutionData: NewExecutionData(state, start, end),
+	}
+}
+
+// Initialize begins the setup phase for ShareDistribution.
+// We construct our commitments and encrypted shares before
+// submitting them to the associated smart contract.
+func (t *ShareDistributionTask) Initialize(ctx context.Context, logger *logrus.Entry, eth interfaces.Ethereum, state interface{}) error {
+
+	logger.Infof("ShareDistributionTask Initialize()")
+
+	dkgData, ok := state.(objects.ETHDKGTaskData)
+	if !ok {
+		return objects.ErrCanNotContinue
+	}
+
+	unlock := dkgData.LockState()
+	defer unlock()
+	if dkgData.State != t.State {
+		t.State = dkgData.State
+	}
+
+	if t.State.Phase != objects.ShareDistribution {
+		return fmt.Errorf("%w because it's not in ShareDistribution phase", objects.ErrCanNotContinue)
+	}
+
+	if t.State.SecretValue == nil {
+
+		participants := t.State.GetSortedParticipants()
+		numParticipants := len(participants)
+		threshold := math.ThresholdForUserCount(numParticipants)
+
+		// Generate shares
+		encryptedShares, privateCoefficients, commitments, err := math.GenerateShares(
+			t.State.TransportPrivateKey, participants)
+		if err != nil {
+			logger.Errorf("Failed to generate shares: %v %#v", err, participants)
+			return err
+		}
+
+		// Store calculated values
+		t.State.Participants[t.State.Account.Address].Commitments = commitments
+		t.State.Participants[t.State.Account.Address].EncryptedShares = encryptedShares
+
+		t.State.PrivateCoefficients = privateCoefficients
+		t.State.SecretValue = privateCoefficients[0]
+		t.State.ValidatorThreshold = threshold
+
+		unlock()
+		dkgData.PersistStateCB()
+	} else {
+		logger.Infof("ShareDistributionTask Initialize(): encrypted shares already defined")
+	}
+
+	return nil
+}
+
+// DoWork is the first attempt at distributing shares via ethdkg
+func (t *ShareDistributionTask) DoWork(ctx context.Context, logger *logrus.Entry, eth interfaces.Ethereum) error {
+	return t.doTask(ctx, logger, eth)
+}
+
+// DoRetry is subsequent attempts at distributing shares via ethdkg
+func (t *ShareDistributionTask) DoRetry(ctx context.Context, logger *logrus.Entry, eth interfaces.Ethereum) error {
+	return t.doTask(ctx, logger, eth)
+}
+
+func (t *ShareDistributionTask) doTask(ctx context.Context, logger *logrus.Entry, eth interfaces.Ethereum) error {
+	// Setup
+	t.State.Lock()
+	defer t.State.Unlock()
+
+	logger.Info("ShareDistributionTask doTask()")
+
+	c := eth.Contracts()
+	me := t.State.Account.Address
+	logger.Debugf("me:%v", me.Hex())
+
+	// Setup
+	txnOpts, err := eth.GetTransactionOpts(ctx, t.State.Account)
+	if err != nil {
+		return dkg.LogReturnErrorf(logger, "getting txn opts failed: %v", err)
+	}
+
+	// If the TxOpts exists, meaning the Tx replacement timeout was reached,
+	// we increase the Gas to have priority for the next blocks
+	if t.TxOpts != nil && t.TxOpts.Nonce != nil {
+		logger.Info("txnOpts Replaced")
+		txnOpts.Nonce = t.TxOpts.Nonce
+		txnOpts.GasFeeCap = t.TxOpts.GasFeeCap
+		txnOpts.GasTipCap = t.TxOpts.GasTipCap
+	}
+
+	// Distribute shares
+	txn, err := c.Ethdkg().DistributeShares(txnOpts, t.State.Participants[me].EncryptedShares, t.State.Participants[me].Commitments)
+	if err != nil {
+		logger.Errorf("distributing shares failed: %v", err)
+		return err
+	}
+	t.TxOpts.TxHashes = append(t.TxOpts.TxHashes, txn.Hash())
+	t.TxOpts.GasFeeCap = txn.GasFeeCap()
+	t.TxOpts.GasTipCap = txn.GasTipCap()
+	t.TxOpts.Nonce = big.NewInt(int64(txn.Nonce()))
+
+	logger.WithFields(logrus.Fields{
+		"GasFeeCap": t.TxOpts.GasFeeCap,
+		"GasTipCap": t.TxOpts.GasTipCap,
+		"Nonce":     t.TxOpts.Nonce,
+	}).Info("share distribution fees")
+
+	// Queue transaction
+	eth.Queue().QueueTransaction(ctx, txn)
+	t.Success = true
+
+	return nil
+}
+
+// ShouldRetry checks if it makes sense to try again
+// if the DKG process is in the right phase and blocks
+// range and the distributed share hash is empty, we retry
+func (t *ShareDistributionTask) ShouldRetry(ctx context.Context, logger *logrus.Entry, eth interfaces.Ethereum) bool {
+	t.State.Lock()
+	defer t.State.Unlock()
+
+	logger.Info("ShareDistributionTask ShouldRetry()")
+
+	// This wraps the retry logic for the general case
+	generalRetry := GeneralTaskShouldRetry(ctx, logger, eth, t.Start, t.End)
+	if !generalRetry {
+		return false
+	}
+
+	if t.State.Phase != objects.ShareDistribution {
+		return false
+	}
+
+	// If it's generally good to retry, let's try to be more specific
+	callOpts := eth.GetCallOpts(ctx, t.State.Account)
+	participantState, err := eth.Contracts().Ethdkg().GetParticipantInternalState(callOpts, t.State.Account.Address)
+	if err != nil {
+		logger.Errorf("ShareDistributionTask.ShoudRetry() unable to GetParticipantInternalState(): %v", err)
+		return true
+	}
+
+	logger.Infof("DistributionHash: %x", participantState.DistributedSharesHash)
+	var emptySharesHash [32]byte
+	if participantState.DistributedSharesHash == emptySharesHash {
+		logger.Warn("Did not distribute shares after all. needs retry")
+		return true
+	}
+
+	logger.Info("Did distribute shares after all. needs no retry")
+
+	return false
+}
+
+// DoDone creates a log entry saying task is complete
+func (t *ShareDistributionTask) DoDone(logger *logrus.Entry) {
+	t.State.Lock()
+	defer t.State.Unlock()
+
+	logger.WithField("Success", t.Success).Infof("ShareDistributionTask done")
+}
+
+func (t *ShareDistributionTask) GetExecutionData() interface{} {
+	return t.ExecutionData
+}
---
+++ layer1/dkg/dkgtasks/task_retry.go
@@ -0,0 +1,77 @@
+package dkgtasks
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/alicenet/alicenet/layer1/interfaces"
+	"github.com/alicenet/alicenet/utils"
+	"github.com/sirupsen/logrus"
+)
+
+// GeneralTaskShouldRetry is the general logic used to determine if a task should try again
+// -- Process is
+func GeneralTaskShouldRetry(ctx context.Context, logger *logrus.Entry,
+	eth interfaces.Ethereum, expectedFirstBlock uint64, expectedLastBlock uint64) bool {
+
+	result := internalGeneralTaskShouldRetry(ctx, logger, eth, expectedFirstBlock, expectedLastBlock)
+
+	logger.Infof("GeneralTaskRetry(expectedFirstBlock: %v expectedLastBlock:%v): %v",
+		expectedFirstBlock,
+		expectedLastBlock,
+		result)
+
+	return result
+}
+
+// FormatPublicKey formats the public key suitably for logging
+func FormatPublicKey(publicKey [2]*big.Int) string {
+	pk0BytesRaw := publicKey[0].Bytes()
+	pk1BytesRaw := publicKey[1].Bytes()
+	pk0Bytes := utils.ForceSliceToLength(pk0BytesRaw, 32)
+	pk1Bytes := utils.ForceSliceToLength(pk1BytesRaw, 32)
+	pk0Hex := utils.EncodeHexString(pk0Bytes)
+	pk1Hex := utils.EncodeHexString(pk1Bytes)
+	pk0 := pk0Hex[0:3]
+	pk1 := pk1Hex[len(pk1Hex)-3:]
+	return fmt.Sprintf("0x%v...%v", pk0, pk1)
+}
+
+// FormatBigIntSlice formats a slice of *big.Int's suitably for logging
+func FormatBigIntSlice(slice []*big.Int) string {
+	var b bytes.Buffer
+	for _, i := range slice {
+		b.WriteString(i.Text(16))
+	}
+
+	str := b.String()
+
+	if len(str) < 16 {
+		return fmt.Sprintf("0x%v", str)
+	}
+
+	return fmt.Sprintf("0x%v...%v", str[0:3], str[len(str)-3:])
+}
+
+func internalGeneralTaskShouldRetry(ctx context.Context, logger *logrus.Entry,
+	eth interfaces.Ethereum, expectedFirstBlock uint64, expectedLastBlock uint64) bool {
+
+	// Make sure we're in the right block range to continue
+	currentBlock, err := eth.GetCurrentHeight(ctx)
+	if err != nil {
+		// This probably means an endpoint issue, so we have to try again
+		logger.Warnf("could not check current height of chain: %v", err)
+		return true
+	}
+
+	if currentBlock >= expectedLastBlock {
+		return false
+	}
+
+	// TODO Any other general cases where we know retry won't work?
+
+	// We won't loop forever because eventually currentBlock > expectedLastBlock
+	return true
+}
