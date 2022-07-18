bbe8e9461692f3c89538d94479ff8998b88c7040
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-07-18 09:05:59 -0300

    Update docs

--- layer1/executor/tasks/base_task.go
+++ layer1/executor/tasks/base_task.go
@@ -67,6 +67,9 @@ type BaseTask struct {
 	taskResponseChan    TaskResponseChan              `json:"-"`
 }
 
+// NewBaseTask creates a new Base task. BaseTask should be the base of any task.
+// This function is called outside the scheduler o create the object to be
+// scheduled.
 func NewBaseTask(start uint64, end uint64, allowMultiExecution bool, subscribeOptions *transaction.SubscribeOptions) *BaseTask {
 	ctx, cf := context.WithCancel(context.Background())
 
@@ -80,7 +83,9 @@ func NewBaseTask(start uint64, end uint64, allowMultiExecution bool, subscribeOp
 	}
 }
 
-// Initialize default implementation for the ITask interface
+// Initialize initializes the task after its creation. It should be only called
+// by the task scheduler during task spawn as separated go routine. This
+// function all the parameters for task execution and control by the scheduler.
 func (bt *BaseTask) Initialize(ctx context.Context, cancelFunc context.CancelFunc, database *db.Database, logger *logrus.Entry, eth layer1.Client, contracts layer1.AllSmartContracts, name string, id string, taskResponseChan TaskResponseChan) error {
 	bt.Lock()
 	defer bt.Unlock()
@@ -102,89 +107,96 @@ func (bt *BaseTask) Initialize(ctx context.Context, cancelFunc context.CancelFun
 	return nil
 }
 
-// GetId default implementation for the ITask interface
+// GetId gets the task unique ID.
 func (bt *BaseTask) GetId() string {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.Id
 }
 
-// GetStart default implementation for the ITask interface
+// GetStart gets the start date of a task. Returns 0 if a task does not has a
+// start date (started immediately).
 func (bt *BaseTask) GetStart() uint64 {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.Start
 }
 
-// GetEnd default implementation for the ITask interface
+// GetEnd gets the end date in blocks of a task. In case 0, the task does not
+// has a end block.
 func (bt *BaseTask) GetEnd() uint64 {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.End
 }
 
-// GetName default implementation for the ITask interface
+// GetName get the name of the task.
 func (bt *BaseTask) GetName() string {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.Name
 }
 
-// GetAllowMultiExecution default implementation for the ITask interface
+// GetAllowMultiExecution returns if a task type allows multiple execution.
 func (bt *BaseTask) GetAllowMultiExecution() bool {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.AllowMultiExecution
 }
 
+// GetSubscribeOptions gets the transactionWatcher subscribeOptions specific for
+// a task.
 func (bt *BaseTask) GetSubscribeOptions() *transaction.SubscribeOptions {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.SubscribeOptions
 }
 
-// GetCtx default implementation for the ITask interface
+// GetCtx get the context to be used by a task.
 func (bt *BaseTask) GetCtx() context.Context {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.ctx
 }
 
-// GetCtx default implementation for the ITask interface
+// WasKilled returns true if the task was killed otherwise false.
 func (bt *BaseTask) WasKilled() bool {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.wasKilled
 }
 
-// GetEth default implementation for the ITask interface
+// GetClient returns the layer1 client implemented by the task.
 func (bt *BaseTask) GetClient() layer1.Client {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.client
 }
 
-// GetEth default implementation for the ITask interface
+// GetContractsHandler returns the handler that has access to all different
+// layer1 smart contracts.
 func (bt *BaseTask) GetContractsHandler() layer1.AllSmartContracts {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.contracts
 }
 
-// GetLogger default implementation for the ITask interface
+// GetLogger returns the task logger.
 func (bt *BaseTask) GetLogger() *logrus.Entry {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.logger
 }
 
+// GetDB returns the database where the task can save and load its state.
 func (bt *BaseTask) GetDB() *db.Database {
 	bt.RLock()
 	defer bt.RUnlock()
 	return bt.database
 }
 
-// Close default implementation for the ITask interface
+// Close closes a running task. It set a bool flag and call the cancelFunc in
+// case its different from nil.
 func (bt *BaseTask) Close() {
 	bt.Lock()
 	defer bt.Unlock()
@@ -194,7 +206,7 @@ func (bt *BaseTask) Close() {
 	bt.wasKilled = true
 }
 
-// Finish default implementation for the ITask interface
+// Finish executes the clean up logic once a task finishes.
 func (bt *BaseTask) Finish(err error) {
 	if err != nil {
 		if bt.wasKilled {
