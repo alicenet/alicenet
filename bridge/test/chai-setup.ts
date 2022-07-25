12b8f8b5ef848642038f92917d5a4d15d89a6790
Keiron <keiron.gulrajani@gmail.com>
2022-07-25 17:10:56 +0100

    Installed hardhat chai matchers Removed redundant/clashing libs

--- bridge/test/chai-setup.ts
+++ bridge/test/chai-setup.ts
@@ -1,9 +1,5 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { chaiEthers } from "chai-ethers";
-import { solidity } from "ethereum-waffle";
-chai.use(solidity);
-chai.use(chaiEthers);
 chai.use(chaiAsPromised);
 chai.config.includeStack = true;
 export = chai;
