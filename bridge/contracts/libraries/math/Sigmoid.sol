9c6b609992d4359258f4001e88b233601e96da3b
Chris Gorman <chgorman@users.noreply.github.com>
2022-07-30 16:17:03 -0700

    Saving changes.

--- bridge/contracts/libraries/math/Sigmoid.sol
+++ bridge/contracts/libraries/math/Sigmoid.sol
@@ -54,42 +54,42 @@ abstract contract Sigmoid {
     /// @param x The uint256 number for which to calculate the square root.
     /// @return result The result as an uint256.
     function _sqrt(uint256 x) internal pure returns (uint256 result) {
+        if (x == 0) {
+            return x;
+        }
+
+        // Set the initial guess to the closest power of two that is higher than x.
+        uint256 xAux = uint256(x);
+        result = 1;
+        if (xAux >= 0x100000000000000000000000000000000) {
+            xAux >>= 128;
+            result <<= 64;
+        }
+        if (xAux >= 0x10000000000000000) {
+            xAux >>= 64;
+            result <<= 32;
+        }
+        if (xAux >= 0x100000000) {
+            xAux >>= 32;
+            result <<= 16;
+        }
+        if (xAux >= 0x10000) {
+            xAux >>= 16;
+            result <<= 8;
+        }
+        if (xAux >= 0x100) {
+            xAux >>= 8;
+            result <<= 4;
+        }
+        if (xAux >= 0x10) {
+            xAux >>= 4;
+            result <<= 2;
+        }
+        if (xAux >= 0x4) {
+            result <<= 1;
+        }
+
         unchecked {
-            if (x <= 1) {
-                return x;
-            }
-
-            // Set the initial guess to the closest power of two that is higher than x.
-            uint256 xAux = uint256(x);
-            result = 1;
-            if (xAux >= 0x100000000000000000000000000000000) {
-                xAux >>= 128;
-                result <<= 64;
-            }
-            if (xAux >= 0x10000000000000000) {
-                xAux >>= 64;
-                result <<= 32;
-            }
-            if (xAux >= 0x100000000) {
-                xAux >>= 32;
-                result <<= 16;
-            }
-            if (xAux >= 0x10000) {
-                xAux >>= 16;
-                result <<= 8;
-            }
-            if (xAux >= 0x100) {
-                xAux >>= 8;
-                result <<= 4;
-            }
-            if (xAux >= 0x10) {
-                xAux >>= 4;
-                result <<= 2;
-            }
-            if (xAux >= 0x4) {
-                result <<= 1;
-            }
-
         // The operations can never overflow because the result is max 2^127 when it enters this block.
             result = (result + x / result) >> 1;
             result = (result + x / result) >> 1;
