653d1b41a9c45ebd567c4284916399b5b80ecf7e
Chris Gorman <chgorman@users.noreply.github.com>
2022-07-31 14:18:25 -0700

    Cleaned up integer square root code

--- bridge/contracts/libraries/math/Sigmoid.sol
+++ bridge/contracts/libraries/math/Sigmoid.sol
@@ -50,47 +50,23 @@ abstract contract Sigmoid {
             if (x <= 1) {
                 return x;
             }
-            if (x >= 0xfffffffffffffffffffffffffffffffe00000000000000000000000000000001) {
+            if (x >= ((1 << 128) - 1) ** 2) {
                 return (1 << 128) - 1;
             }
             // Here, e represents the bit length;
             // its value is at most 256, so it could fit in a uint16.
             uint256 e = 1;
-            // Here, result is a copy of x
+            // Here, result is a copy of x to compute the bit length
             uint256 result = x;
-            if (result >= 0x100000000000000000000000000000000) {
-                result >>= 128;
-                e += 128;
-            }
-            if (result >= 0x10000000000000000) {
-                result >>= 64;
-                e += 64;
-            }
-            if (result >= 0x100000000) {
-                result >>= 32;
-                e += 32;
-            }
-            if (result >= 0x10000) {
-                result >>= 16;
-                e += 16;
-            }
-            if (result >= 0x100) {
-                result >>= 8;
-                e += 8;
-            }
-            if (result >= 0x10) {
-                result >>= 4;
-                e += 4;
-            }
-            if (result >= 0x4) {
-                result >>= 2;
-                e += 2;
-            }
-            if (result >= 0x2) {
-                e += 1;
-            }
-
-            // e is currently bit length; we overwrite it now
+            if (result >= (1 << 128)) { result >>= 128; e += 128; }
+            if (result >= (1 << 64 )) { result >>= 64;  e += 64;  }
+            if (result >= (1 << 32 )) { result >>= 32;  e += 32;  }
+            if (result >= (1 << 16 )) { result >>= 16;  e += 16;  }
+            if (result >= (1 << 8  )) { result >>= 8;   e += 8;   }
+            if (result >= (1 << 4  )) { result >>= 4;   e += 4;   }
+            if (result >= (1 << 2  )) { result >>= 2;   e += 2;   }
+            if (result >= (1 << 1  )) {                 e += 1;   }
+            // e is currently bit length; we overwrite it to scale x
             e = (256 - e) >> 1;
             // m now satisfies 2**254 <= m < 2**256
             uint256 m = x << (2*e);
