341873ca3bd59c9be6f0a79b03ef4e55f2e974ad
Chris Gorman <chgorman@users.noreply.github.com>
2022-08-03 08:27:04 -0700

    Ran linter.

--- bridge/contracts/libraries/math/Sigmoid.sol
+++ bridge/contracts/libraries/math/Sigmoid.sol
@@ -50,7 +50,7 @@ abstract contract Sigmoid {
             if (x <= 1) {
                 return x;
             }
-            if (x >= ((1 << 128) - 1) ** 2) {
+            if (x >= ((1 << 128) - 1)**2) {
                 return (1 << 128) - 1;
             }
             // Here, e represents the bit length;
@@ -58,28 +58,51 @@ abstract contract Sigmoid {
             uint256 e = 1;
             // Here, result is a copy of x to compute the bit length
             uint256 result = x;
-            if (result >= (1 << 128)) { result >>= 128; e += 128; }
-            if (result >= (1 << 64 )) { result >>= 64;  e += 64;  }
-            if (result >= (1 << 32 )) { result >>= 32;  e += 32;  }
-            if (result >= (1 << 16 )) { result >>= 16;  e += 16;  }
-            if (result >= (1 << 8  )) { result >>= 8;   e += 8;   }
-            if (result >= (1 << 4  )) { result >>= 4;   e += 4;   }
-            if (result >= (1 << 2  )) { result >>= 2;   e += 2;   }
-            if (result >= (1 << 1  )) {                 e += 1;   }
+            if (result >= (1 << 128)) {
+                result >>= 128;
+                e += 128;
+            }
+            if (result >= (1 << 64)) {
+                result >>= 64;
+                e += 64;
+            }
+            if (result >= (1 << 32)) {
+                result >>= 32;
+                e += 32;
+            }
+            if (result >= (1 << 16)) {
+                result >>= 16;
+                e += 16;
+            }
+            if (result >= (1 << 8)) {
+                result >>= 8;
+                e += 8;
+            }
+            if (result >= (1 << 4)) {
+                result >>= 4;
+                e += 4;
+            }
+            if (result >= (1 << 2)) {
+                result >>= 2;
+                e += 2;
+            }
+            if (result >= (1 << 1)) {
+                e += 1;
+            }
             // e is currently bit length; we overwrite it to scale x
             e = (256 - e) >> 1;
             // m now satisfies 2**254 <= m < 2**256
-            uint256 m = x << (2*e);
+            uint256 m = x << (2 * e);
             // result now stores the result
             result = 1 + (m >> 254);
-            result = (result << 1)  + (m >> 251) / result;
-            result = (result << 3)  + (m >> 245) / result;
-            result = (result << 7)  + (m >> 233) / result;
+            result = (result << 1) + (m >> 251) / result;
+            result = (result << 3) + (m >> 245) / result;
+            result = (result << 7) + (m >> 233) / result;
             result = (result << 15) + (m >> 209) / result;
             result = (result << 31) + (m >> 161) / result;
-            result = (result << 63) + (m >>  65) / result;
+            result = (result << 63) + (m >> 65) / result;
             result >>= e;
-            return result*result <= x ? result : (result-1);
+            return result * result <= x ? result : (result - 1);
         }
     }
 }
