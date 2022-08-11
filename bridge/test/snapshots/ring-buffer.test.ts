c70cea6b41da14a841221b7c77118b8a53dc778d
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-08-11 15:28:28 -0300

    Add more unit tests

--- bridge/test/snapshots/ring-buffer.test.ts
+++ bridge/test/snapshots/ring-buffer.test.ts
@@ -159,7 +159,6 @@ contract("SnapshotRingBuffer 0state", async () => {
           { gasLimit: 30000000 }
         );
         await contractTx.wait();
-        // console.log(`epoch: ${i}, gas: ${receipt.gasUsed}`);
       }
       epochs = (await snapshotsG1.getEpoch()).toNumber();
       const lastSnapshot = await snapshotsG1.getLatestSnapshot();
@@ -169,7 +168,172 @@ contract("SnapshotRingBuffer 0state", async () => {
       expect(endBuffShot.blockClaims.height).to.eq((epochs - 5) * epochLength);
     });
 
-    it("attempts to get a snapshot that is no longer in the buffer", async () => {
+    it("getter functions should be always able to work at epoch 0", async () => {
+      let epochs = (await fixture.snapshots.getEpoch()).toNumber();
+      expect(epochs).to.be.equal(0);
+      const lastSnapshot = await fixture.snapshots.getLatestSnapshot();
+      expect(lastSnapshot.blockClaims.height).to.equal(0);
+      let zeroSnapshot = await snapshots.getSnapshot(0);
+      expect(zeroSnapshot.blockClaims.height).to.equal(0);
+      expect(zeroSnapshot.blockClaims.chainId).to.eq(0);
+      expect(zeroSnapshot.committedAt).to.eq(0);
+      expect(
+        await fixture.snapshots.getAliceNetHeightFromSnapshot(0)
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getBlockClaimsFromSnapshot(0)
+      ).to.be.deep.equals([
+        0,
+        0,
+        0,
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      ]);
+      expect(await fixture.snapshots.getChainIdFromSnapshot(0)).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getCommittedHeightFromSnapshot(0)
+      ).to.be.equals(0);
+
+      expect(
+        await fixture.snapshots.getAliceNetHeightFromLatestSnapshot()
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getBlockClaimsFromLatestSnapshot()
+      ).to.be.deep.equals([
+        0,
+        0,
+        0,
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      ]);
+      expect(
+        await fixture.snapshots.getChainIdFromLatestSnapshot()
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getCommittedHeightFromLatestSnapshot()
+      ).to.be.equals(0);
+      const signedSnapshots = signedData1;
+      const validators: Array<string> = [];
+      for (const validator of validatorsSnapshotsG1) {
+        validators.push(validator.address);
+      }
+      const snapshotsG1 = fixture.snapshots.connect(
+        await getValidatorEthAccount(validatorsSnapshotsG1[0])
+      );
+      // take 12 snapshots
+      for (let i = epochs + 1; i <= 12; i++) {
+        await mineBlocks(
+          (await snapshotsG1.getMinimumIntervalBetweenSnapshots()).toBigInt()
+        );
+        const contractTx = await snapshotsG1.snapshot(
+          signedSnapshots[i - 1].GroupSignature,
+          signedSnapshots[i - 1].BClaims,
+          { gasLimit: 30000000 }
+        );
+        await contractTx.wait();
+      }
+
+      zeroSnapshot = await snapshots.getSnapshot(0);
+      expect(zeroSnapshot.blockClaims.height).to.equal(0);
+      expect(zeroSnapshot.blockClaims.chainId).to.eq(0);
+      expect(zeroSnapshot.committedAt).to.eq(0);
+      expect(
+        await fixture.snapshots.getAliceNetHeightFromSnapshot(0)
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getBlockClaimsFromSnapshot(0)
+      ).to.be.deep.equals([
+        0,
+        0,
+        0,
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      ]);
+      expect(await fixture.snapshots.getChainIdFromSnapshot(0)).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getCommittedHeightFromSnapshot(0)
+      ).to.be.equals(0);
+    });
+
+    it("test latest getter functions", async () => {
+      let epochs = (await fixture.snapshots.getEpoch()).toNumber();
+      expect(epochs).to.be.equal(0);
+      let lastSnapshot = await fixture.snapshots.getLatestSnapshot();
+      expect(lastSnapshot.blockClaims.height).to.equal(0);
+      expect(
+        await fixture.snapshots.getAliceNetHeightFromLatestSnapshot()
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getBlockClaimsFromLatestSnapshot()
+      ).to.be.deep.equals([
+        0,
+        0,
+        0,
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      ]);
+      expect(
+        await fixture.snapshots.getChainIdFromLatestSnapshot()
+      ).to.be.equals(0);
+      expect(
+        await fixture.snapshots.getCommittedHeightFromLatestSnapshot()
+      ).to.be.equals(0);
+      const signedSnapshots = signedData1;
+      const validators: Array<string> = [];
+      for (const validator of validatorsSnapshotsG1) {
+        validators.push(validator.address);
+      }
+      const snapshotsG1 = fixture.snapshots.connect(
+        await getValidatorEthAccount(validatorsSnapshotsG1[0])
+      );
+      let lastSnapshotBlockNumber
+      // take 12 snapshots
+      for (let i = epochs + 1; i <= 12; i++) {
+        await mineBlocks(
+          (await snapshotsG1.getMinimumIntervalBetweenSnapshots()).toBigInt()
+        );
+        const contractTx = await snapshotsG1.snapshot(
+          signedSnapshots[i - 1].GroupSignature,
+          signedSnapshots[i - 1].BClaims,
+          { gasLimit: 30000000 }
+        );
+        const rcpt = await contractTx.wait();
+        lastSnapshotBlockNumber = rcpt.blockNumber
+      }
+      epochs = (await snapshotsG1.getEpoch()).toNumber();
+      lastSnapshot = await fixture.snapshots.getLatestSnapshot();
+      expect(lastSnapshot.blockClaims.height).to.equal(epochs * epochLength);
+      expect(
+        await fixture.snapshots.getAliceNetHeightFromLatestSnapshot()
+      ).to.be.equals(epochs * epochLength);
+      expect(
+        await fixture.snapshots.getBlockClaimsFromLatestSnapshot()
+      ).to.be.deep.equals([
+        1,
+        12288,
+        0,
+        "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+        "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+        "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+        "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+      ]);
+      expect(
+        await fixture.snapshots.getChainIdFromLatestSnapshot()
+      ).to.be.equals(1);
+      expect(
+        await fixture.snapshots.getCommittedHeightFromLatestSnapshot()
+      ).to.be.equals(lastSnapshotBlockNumber);
+    });
+
+    it("attempts to get a parameters from snapshot that is no longer in the buffer", async () => {
       let epochs = (await fixture.snapshots.getEpoch()).toNumber();
       const signedSnapshots = signedData1;
       const validators: Array<string> = [];
@@ -179,7 +343,7 @@ contract("SnapshotRingBuffer 0state", async () => {
       const snapshotsG1 = fixture.snapshots.connect(
         await getValidatorEthAccount(validatorsSnapshotsG1[0])
       );
-      // take 6 snapshots
+      // take 12 snapshots
       for (let i = epochs + 1; i <= 12; i++) {
         await mineBlocks(
           (await snapshotsG1.getMinimumIntervalBetweenSnapshots()).toBigInt()
@@ -195,6 +359,24 @@ contract("SnapshotRingBuffer 0state", async () => {
       const lastSnapshot = await snapshotsG1.getLatestSnapshot();
       expect(lastSnapshot.blockClaims.height).to.equal(epochs * epochLength);
       await expect(snapshots.getSnapshot(epochs - 6))
+        .to.be.revertedWithCustomError(
+          fixture.snapshots,
+          `SnapshotsNotInBuffer`
+        )
+        .withArgs(epochs - 6);
+        await expect(snapshots.getAliceNetHeightFromSnapshot(epochs - 6))
+        .to.be.revertedWithCustomError(
+          fixture.snapshots,
+          `SnapshotsNotInBuffer`
+        )
+        .withArgs(epochs - 6);
+        await expect(snapshots.getBlockClaimsFromSnapshot(epochs - 6))
+        .to.be.revertedWithCustomError(
+          fixture.snapshots,
+          `SnapshotsNotInBuffer`
+        )
+        .withArgs(epochs - 6);
+        await expect(snapshots.getCommittedHeightFromSnapshot(epochs - 6))
         .to.be.revertedWithCustomError(
           fixture.snapshots,
           `SnapshotsNotInBuffer`
