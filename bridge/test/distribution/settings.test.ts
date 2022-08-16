77db476ca8339821b7350eec3edabf03d1992cfa
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-08-16 19:38:43 -0300

    Add more unit tests

--- bridge/test/distribution/settings.test.ts
+++ bridge/test/distribution/settings.test.ts
@@ -1,3 +1,4 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber } from "ethers";
 import { ethers, expect } from "hardhat";
 import { getState, showState } from "../bToken/setup";
@@ -5,9 +6,11 @@ import { Fixture, getFixture } from "../setup";
 
 describe("Testing splits settings update", async () => {
   let fixture: Fixture;
+  let admin: SignerWithAddress;
 
   beforeEach(async function () {
     fixture = await getFixture();
+    [admin] = await ethers.getSigners();
     showState("Initial", await getState(fixture));
   });
 
@@ -61,5 +64,21 @@ describe("Testing splits settings update", async () => {
       BigNumber.from(300),
       BigNumber.from(100),
     ]);
+  });
+
+  it("Only BToken should be able to distribute", async () => {
+    await expect(
+      fixture.distribution.connect(admin).depositEth(42, { value: 10000 })
+    )
+      .to.revertedWithCustomError(fixture.distribution, "OnlyBToken")
+      .withArgs(admin.address, fixture.bToken.address);
+    const rcpt = await (await fixture.bToken.distribute()).wait();
+    expect(rcpt.status).to.be.equals(1);
+  });
+
+  it("Should not be able to send ethereum", async () => {
+    await expect(
+      admin.sendTransaction({ to: fixture.distribution.address, value: 10000 })
+    ).to.reverted;
   });
 });
