08f9027db68bbda7872a863976a813783f6acf6e
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-08-16 11:49:13 -0300

    Add new version of circuit breaker

--- bridge/contracts/AToken.sol
+++ bridge/contracts/AToken.sol
@@ -6,18 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./utils/Admin.sol";
 import "./utils/ImmutableAuth.sol";
 import "contracts/interfaces/IAToken.sol";
+import "contracts/utils/CircuitBreaker.sol";
 
 /// @custom:salt AToken
 /// @custom:deploy-type deployStatic
 contract AToken is
     IAToken,
     ERC20Upgradeable,
+    CircuitBreaker,
     ImmutableFactory,
     ImmutableATokenMinter,
     ImmutableATokenBurner
 {
-
-    uint256 internal constant CONVERSION_MULTIPLIER = 10;
+    uint256 internal constant CONVERSION_MULTIPLIER = 15;
     uint256 internal constant CONVERSION_SCALE = 10;
     address internal immutable _legacyToken;
     bool internal _migrationAllowed;
@@ -37,12 +38,17 @@ contract AToken is
     function migrate(uint256 amount) public {
         require(_migrationAllowed, "MadTokens migration not allowed");
         IERC20(_legacyToken).transferFrom(msg.sender, address(this), amount);
-        uint256 scaledAmount = amount * CONVERSION_MULTIPLIER / CONVERSION_SCALE;
+        uint256 scaledAmount = (amount * CONVERSION_MULTIPLIER) / CONVERSION_SCALE;
         _mint(msg.sender, scaledAmount);
     }
 
     function allowMigration() public onlyFactory {
         _migrationAllowed = true;
+    }
+
+    /// tripCB opens the circuit breaker, may only be called by the factory
+    function tripCB() public onlyFactory {
+        _tripCB();
     }
 
     function externalMint(address to, uint256 amount) public onlyATokenMinter {
