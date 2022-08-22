4de7606994a617be70e1937008b270ff9e1077d3
zjlin <zijunlin32@gmail.com>
2022-08-22 14:36:04 -0400

    Change pragma to 0.8.16 (#199)
    * changed pragma to ^0.8.16
    
    * changed solidity version in  prettierRC
    
    * lint and format
    
    * generated bindings
    
    * xit deploys all contract cli test

--- bridge/contracts/utils/Admin.sol
+++ bridge/contracts/utils/Admin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT-open-group
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
 import "contracts/libraries/errors/AdminErrors.sol";
 
 abstract contract Admin {
--- bridge/contracts/utils/MagicEthTransfer.sol
+++ bridge/contracts/utils/MagicEthTransfer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT-open-group
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
 
 import "contracts/utils/MagicValue.sol";
 import "contracts/interfaces/IMagicEthTransfer.sol";
