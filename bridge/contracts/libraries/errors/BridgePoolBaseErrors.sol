2c37488e4dd283266399e79eda27cceda6b470e2
Gustavo Vazquez Amado <gustavo@madhive.com>
2022-09-09 16:52:37 +0000

    Added BridgePoolBaseErrors

---
+++ bridge/contracts/libraries/errors/BridgePoolBaseErrors.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT-open-group
+pragma solidity ^0.8.16;
+
+library BridgePoolBaseErrors {
+    error OnlyBridgePool();
+    error ReceiverIsNotOwnerOnProofOfBurnUTXO();
+}
