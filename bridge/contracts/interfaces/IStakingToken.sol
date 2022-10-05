4947da5b45837c260a3279324d03f770d27f3104
ZJ <zijunlin32@gmail.com>
2022-10-05 19:36:08 -0400

    clean up

--- bridge/contracts/interfaces/IStakingToken.sol
+++ bridge/contracts/interfaces/IStakingToken.sol
@@ -3,16 +3,7 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IStakingToken {
-    
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
 
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function balanceOf(address acct) external view returns (uint256 balance);
     
     function migrate(uint256 amount) external;
 
