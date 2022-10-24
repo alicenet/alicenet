06aaf4ad19321d10ac0acf0ea9cdd98b3c487ae5
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-10-24 14:59:04 -0400

    Revert "Add allow-migration alicenetFactoryTasks.ts"
    This reverts commit a409899c046ca6c11b6f86e4f15f069da9a05ee5.

--- bridge/scripts/lib/alicenetFactoryTasks.ts
+++ bridge/scripts/lib/alicenetFactoryTasks.ts
@@ -83,15 +83,6 @@ import {
   updateProxyList,
 } from "./deployment/factoryStateUtil";
 
-
-task("allow-migration", "Sets the AToken migration = true")
-    .addParam("factoryAddress", "address of the factory deploying the contract")
-    .setAction(async (taskArgs, hre) => {
-        try {
-            await allowMigration(hre, taskArgs.factoryAddress);
-        } catch (error) { }
-    });
-
 task(
   "get-network",
   "gets the current network being used from provider"
@@ -1196,27 +1187,6 @@ task(
     console.log(`total gas used: ${cumulativeGasUsed.toString()}`);
   });
 
-  async function allowMigration(
-    hre: HardhatRuntimeEnvironment,
-    factoryAddress: string,
-): Promise<true> {
-    const factory = await hre.ethers.getContractAt(
-        "AliceNetFactory",
-        factoryAddress
-    );
-    const [admin] = await hre.ethers.getSigners();
-    const aTokenAddress = await factory.callStatic.lookup(
-        hre.ethers.utils.formatBytes32String("AToken")
-    );
-    const aToken = await hre.ethers.getContractAt(
-        "AToken",
-        aTokenAddress
-    )
-    const calldata = aToken.interface.encodeFunctionData("allowMigration");
-    let tx = await factory.connect(admin).callAny(aTokenAddress, 0, calldata);
-    await tx.wait();
-    return true;
-}
 async function checkUserDirPath(path: string) {
   if (path !== undefined) {
     if (!fs.existsSync(path)) {
