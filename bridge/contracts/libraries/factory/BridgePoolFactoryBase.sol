694adb7f4822779a0f38000e188902f2835e42a2
Gustavo Vazquez <gusjavaz@gmail.com>
2022-11-11 12:54:43 +0000

    Added alicenet factory as BP constructor parameter

--- bridge/contracts/libraries/factory/BridgePoolFactoryBase.sol
+++ bridge/contracts/libraries/factory/BridgePoolFactoryBase.sol
@@ -82,10 +82,15 @@ abstract contract BridgePoolFactoryBase is ImmutableFactory {
         uint32 codeSize;
         bool native = true;
         uint16 version;
+        bytes memory alicenetFactoryAddress = abi.encodePacked(
+            bytes32(uint256(uint160(_factoryAddress())))
+        );
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, deployCode_.offset, deployCode_.length)
-            addr := create(value_, ptr, deployCode_.length)
+            // add bytes32 alicenet factory address as parameter to constructor
+            mstore(add(ptr, deployCode_.length), alicenetFactoryAddress)
+            addr := create(value_, ptr, add(deployCode_.length, 32))
             codeSize := extcodesize(addr)
         }
         if (codeSize == 0) {
