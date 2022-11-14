ee04520b875630cfc7d68a6f87259a037947e1cc
diligentis <97410946+diligentis@users.noreply.github.com>
2022-11-14 10:43:33 -0500

    Another flakey test.
    - Part of #483

--- layer1/executor/task_handler_test.go
+++ layer1/executor/task_handler_test.go
@@ -106,6 +106,10 @@ func TestTasksHandlerAndManager_Schedule_WrongStartDate(t *testing.T) {
 }
 
 func TestTasksHandlerAndManager_Schedule_WrongEndDate(t *testing.T) {
+	if env := os.Getenv("RUN_FLAKES"); env == "" {
+		t.Skip("flakey test skipped unless RUN_FLAKES environment is set to something")
+	}
+
 	handler, client, _, _, _ := getTaskHandler(t, true)
 	client.GetFinalizedHeightFunc.SetDefaultReturn(12, nil)
 	handler.Start()
