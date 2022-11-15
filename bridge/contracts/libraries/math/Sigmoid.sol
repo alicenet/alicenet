9e64785c4e0febf3b26cccad08aafe415629cef1
Chris Gorman <chgorman@users.noreply.github.com>
2022-11-15 16:05:25 -0700

    Updated isqrt function to reduce gas costs; added comments for clarity.

--- bridge/contracts/libraries/math/Sigmoid.sol
+++ bridge/contracts/libraries/math/Sigmoid.sol
@@ -45,63 +45,70 @@ abstract contract Sigmoid {
         return b_;
     }
 
-    function _sqrt(uint256 x) internal pure returns (uint256) {
+    // _sqrt implements an algorithm for computing integer square roots
+    // based on Newton's method for computing square roots.
+    // See https://github.com/alicenet/.github/wiki/technical-articles
+    // for an article discussing computing integer square roots in Solidity
+    // as well as an explanation about these steps.
+    function _sqrt(uint256 x) internal pure returns (uint256 result) {
         unchecked {
+            // Take care of integer square root edge cases.
+            // Naturally, Isqrt(0) == 0, Isqrt(1) == 1,
+            // and Isqrt(x) == (1 << 128) - 1
+            // for ((1<<128)-1)**2 <= x < 2**256
             if (x <= 1) {
                 return x;
             }
             if (x >= ((1 << 128) - 1)**2) {
                 return (1 << 128) - 1;
             }
-            // Here, e represents the bit length;
-            // its value is at most 256, so it could fit in a uint16.
-            uint256 e = 1;
-            // Here, result is a copy of x to compute the bit length
-            uint256 result = x;
-            if (result >= (1 << 128)) {
-                result >>= 128;
-                e += 128;
+            // We now compute an initial approximation of Isqrt(x)
+            // (stored in result) by computing the largest power of 2
+            // less than or equal to Isqrt(x).
+            uint256 xAux = x;
+            result = 1;
+            if (xAux >= (1 << 128)) {
+                xAux >>= 128;
+                result <<= 64;
             }
-            if (result >= (1 << 64)) {
-                result >>= 64;
-                e += 64;
+            if (xAux >= (1 << 64)) {
+                xAux >>= 64;
+                result <<= 32;
             }
-            if (result >= (1 << 32)) {
-                result >>= 32;
-                e += 32;
+            if (xAux >= (1 << 32)) {
+                xAux >>= 32;
+                result <<= 16;
             }
-            if (result >= (1 << 16)) {
-                result >>= 16;
-                e += 16;
+            if (xAux >= (1 << 16)) {
+                xAux >>= 16;
+                result <<= 8;
             }
-            if (result >= (1 << 8)) {
-                result >>= 8;
-                e += 8;
+            if (xAux >= (1 << 8)) {
+                xAux >>= 8;
+                result <<= 4;
             }
-            if (result >= (1 << 4)) {
-                result >>= 4;
-                e += 4;
+            if (xAux >= (1 << 4)) {
+                xAux >>= 4;
+                result <<= 2;
             }
-            if (result >= (1 << 2)) {
-                result >>= 2;
-                e += 2;
+            if (xAux >= (1 << 2)) {
+                result <<= 1;
             }
-            if (result >= (1 << 1)) {
-                e += 1;
-            }
-            // e is currently bit length; we overwrite it to scale x
-            e = (256 - e) >> 1;
-            // m now satisfies 2**254 <= m < 2**256
-            uint256 m = x << (2 * e);
-            // result now stores the result
-            result = 1 + (m >> 254);
-            result = (result << 1) + (m >> 251) / result;
-            result = (result << 3) + (m >> 245) / result;
-            result = (result << 7) + (m >> 233) / result;
-            result = (result << 15) + (m >> 209) / result;
-            result = (result << 31) + (m >> 161) / result;
-            result = (result << 63) + (m >> 65) / result;
-            result >>= e;
+            // At this point, result is the largest power of 2
+            // less than or equal to Isqrt(x).
+            // We now compute a better initial approximation of Isqrt(x).
+            result += (result >> 1);
+            // At this point, we perform Newton iterations to compute
+            // more accurate approximations of Isqrt(x).
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            // We perform one potential correction to ensure a correct value.
+            // It is not possible to overflow at this point because
+            // x < ((1 << 128) - 1)**2, so sqrt(x) <= (1 << 128) - 2.
             return result * result <= x ? result : (result - 1);
         }
     }
