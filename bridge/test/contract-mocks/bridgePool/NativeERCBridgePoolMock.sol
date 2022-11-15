2f1b8d52a084dc4a85f9697c3dc0ebd760bcd0b3
Gustavo Vazquez <gusjavaz@gmail.com>
2022-11-15 14:01:50 +0000

    Refactoring

--- bridge/test/contract-mocks/bridgePool/NativeERCBridgePoolMock.sol
+++ bridge/test/contract-mocks/bridgePool/NativeERCBridgePoolMock.sol
@@ -17,15 +17,15 @@ contract NativeERCBridgePoolMock is
         _ercContract = ercContract_;
     }
 
-    function deposit(address msgSender, bytes calldata depositParameters_) public override {
-        super.deposit(msgSender, depositParameters_);
+    function deposit(address sender, bytes calldata depositParameters_) public override {
+        super.deposit(sender, depositParameters_);
     }
 
     function withdraw(
-        address msgSender,
+        address receiver,
         bytes memory vsPreImage,
         bytes[4] memory proofs
-    ) public override returns (address account, uint256 value) {
-        (account, value) = super.withdraw(msgSender, vsPreImage, proofs);
+    ) public override returns (uint256 value) {
+        value = super.withdraw(receiver, vsPreImage, proofs);
     }
 }
