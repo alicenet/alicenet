5e90f4504a5f8b222a0f76788d6385880449a366
sachin sharma <sachin.sharma@tcu.edu>
2022-11-15 23:06:17 -0600

    (fix): Revert to regex logic

--- cmd/testutils/extractor/main.go
+++ cmd/testutils/extractor/main.go
@@ -3,22 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/spf13/viper"
+	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
 	network := flag.String("n", "", "network name.")
-	configPath := flag.String("p", "", "file path.")
+	filePath := flag.String("p", "", "file path.")
 	flag.Parse()
 
-	viper.SetConfigName("factoryState")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(*configPath)
-	err := viper.ReadInConfig()
+	bytes, err := os.ReadFile(*filePath)
 	if err != nil {
-		panic(err)
+		panic("Could not read file")
 	}
-
-	addr := viper.GetString(fmt.Sprintf("%s.defaultFactoryAddress", *network))
-	fmt.Println(addr)
+	outerRegex := regexp.MustCompile(fmt.Sprintf(`\[%s\]\ndefaultFactoryAddress = \".*\"\n`, *network))
+	innerRegex := regexp.MustCompile(`defaultFactoryAddress = .*`)
+	matchedOuter := outerRegex.FindAllSubmatch(bytes, -1)
+	for i := 0; i < len(matchedOuter); i++ {
+		var innerSentence string
+		for j := 0; j < len(matchedOuter[i]); j++ {
+			innerSentence += string(matchedOuter[i][j])
+		}
+		matchedInner := innerRegex.FindAllSubmatch([]byte(innerSentence), -1)
+		tempResult := matchedInner[0]
+		var sentence string
+		for j := 0; j < len(tempResult); j++ {
+			sentence += string(tempResult[j])
+		}
+		splitBySpace := strings.Split(sentence, " ")
+		fmt.Printf("%s", splitBySpace[len(splitBySpace)-1])
+	}
 }
