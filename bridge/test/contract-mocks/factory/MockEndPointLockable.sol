5bef689ae213f1d73cfe241fd5ad981f09007d5e
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2022-11-18 11:49:21 -0300

    Add more unit tests around pulution of storage

--- bridge/test/contract-mocks/factory/MockEndPointLockable.sol
+++ bridge/test/contract-mocks/factory/MockEndPointLockable.sol
@@ -5,6 +5,7 @@ import "contracts/Proxy.sol";
 import "contracts/libraries/proxy/ProxyInternalUpgradeLock.sol";
 import "contracts/libraries/proxy/ProxyInternalUpgradeUnlock.sol";
 import "contracts/libraries/proxy/ProxyImplementationGetter.sol";
+import "hardhat/console.sol";
 
 interface IMockEndPointLockable {
     function addOne() external;
@@ -50,6 +51,19 @@ contract MockEndPointLockable is
     function addTwo() public {
         i = i + 2;
         emit AddedTwo(i);
+    }
+
+    function poluteImplementationAddress() public {
+        assembly ("memory-safe") {
+            let implSlot := not(0x00)
+            sstore(
+                implSlot,
+                or(
+                    0xbabeffff15deffffbabeffff0000000000000000000000000000000000000000,
+                    sload(implSlot)
+                )
+            )
+        }
     }
 
     function upgradeLock() public {
--- bridge/test/proxy/Proxy.test.ts
+++ bridge/test/proxy/Proxy.test.ts
@@ -119,6 +119,68 @@ describe("PROXY", async () => {
     expect(expectedAddress).to.equal(endPoint.address);
   });
 
+  it("locks the proxy upgradeability even with poluted implementation address", async () => {
+    const accounts = await getAccounts();
+    const factory = await deployFactory();
+    const proxyFactory = await ethers.getContractFactory(PROXY);
+    const proxy = await proxyFactory.deploy();
+    const endPointLockableFactory = await ethers.getContractFactory(
+      "MockEndPointLockable"
+    );
+    const endPointLockable = await endPointLockableFactory.deploy(accounts[0]);
+    const endPointFactory = await ethers.getContractFactory(END_POINT);
+    const endPoint = await endPointFactory.deploy();
+    expect(proxy.deployed());
+    const abicoder = new ethers.utils.AbiCoder();
+    let encodedAddress = abicoder.encode(
+      ["address"],
+      [endPointLockable.address]
+    );
+    let txReq = {
+      data: "0xca11c0de11" + encodedAddress.substring(2),
+      gasLimit: 1000000,
+    };
+    const txResponse = await proxy.fallback(txReq);
+    const receipt = await txResponse.wait();
+    expect(receipt.status).to.equal(1);
+    const proxyImplAddr = await getProxyImplementation(proxy.address);
+    expect(proxyImplAddr).to.equal(endPointLockable.address);
+    expect(await factory.getProxyImplementation(proxy.address)).to.equal(
+      endPointLockable.address
+    );
+    // interface of logic connected to logic contract
+    const proxyContract = endPointLockableFactory.attach(proxy.address);
+    await proxyContract.poluteImplementationAddress();
+    expect(proxyImplAddr).to.equal(endPointLockable.address);
+    expect(await factory.getProxyImplementation(proxy.address)).to.equal(
+      endPointLockable.address
+    );
+    // lock the implementation
+    expect((await (await proxyContract.upgradeLock()).wait()).status).to.equal(
+      1
+    );
+    expect(proxyImplAddr).to.equal(endPointLockable.address);
+    expect(await factory.getProxyImplementation(proxy.address)).to.equal(
+      endPointLockable.address
+    );
+    let response = proxy.fallback(txReq);
+    await expect(response).to.be.revertedWith("update locked");
+    expect(
+      (await (await proxyContract.upgradeUnlock()).wait()).status
+    ).to.equal(1);
+    encodedAddress = abicoder.encode(["address"], [endPoint.address]);
+    txReq = {
+      data: "0xca11c0de11" + encodedAddress.substring(2),
+      gasLimit: 1000000,
+    };
+    response = proxy.fallback(txReq);
+    const expectedAddress = await getProxyImplementation(proxy.address);
+    expect(expectedAddress).to.equal(endPoint.address);
+    expect(await factory.getProxyImplementation(proxy.address)).to.equal(
+      endPoint.address
+    );
+  });
+
   it("should not be able to call proxy function with incorrect signature", async () => {
     const proxyFactory = await ethers.getContractFactory(PROXY);
     const proxy = await proxyFactory.deploy();
