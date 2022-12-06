2192336d9701c0db71db827b0ced1c3fd42ff43c
Keiron <keiron.gulrajani@gmail.com>
2022-12-06 15:30:56 +0100

    Added foundry alcb mint tests

---
+++ bridge/test/foundry/alcb/ALCB.mint.t.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "forge-std/Test.sol";
+import "contracts/ALCB.sol";
+import "test/contract-mocks/bToken/BridgeRouterMock.sol";
+import "contracts/libraries/errors/UtilityTokenErrors.sol";
+
+contract ALCBTest is Test {
+    CentralBridgeRouterMock stakingRouter;
+    ALCB alcb;
+    address randomAddress = address(0x000000000000000000000000000000000000dEaD);
+    address randomAddress2 = address(0x00000000000000000000000000000000DeaDBeef);
+    address zeroAddress = address(0x0000000000000000000000000000000000000000);
+    uint256 marketSpread = 4;
+
+    function setUp() public {
+        stakingRouter = new CentralBridgeRouterMock(1000);
+        alcb = new ALCB(address(stakingRouter));
+    }
+
+    function testMintsAlcbFromEtherValueToSender() public {
+        uint256 totalSupplyBefore = alcb.totalSupply();
+        uint256 etherToSend = 4 ether;
+        // fund the address
+        vm.deal(randomAddress, etherToSend);
+        // mock the call to use the address for the next call
+        vm.prank(randomAddress);
+
+        // call the mint function
+        uint256 alcbMinted = alcb.mint{value: etherToSend}(0);
+
+        // check the amount of alcb minted
+        assertEq(402028731704364116575, alcbMinted);
+        assertEq(alcb.balanceOf(randomAddress), alcbMinted);
+        assertEq(alcb.getPoolBalance(), etherToSend / marketSpread);
+        assertEq(alcb.totalSupply(), totalSupplyBefore + alcbMinted);
+    }
+
+    function testMintToMintsAlcbFromEtherValueToSpecifiedAddress() public {
+        uint256 totalSupplyBefore = alcb.totalSupply();
+        uint256 etherToSend = 4 ether;
+        // fund the address
+        vm.deal(randomAddress, etherToSend);
+        // mock the call to use the address for the next call
+        vm.prank(randomAddress);
+
+        // call the mint function
+        uint256 alcbMinted = alcb.mintTo{value: etherToSend}(randomAddress2, 0);
+
+        // check the amount of alcb minted
+        assertEq(402028731704364116575, alcbMinted);
+        assertEq(alcb.balanceOf(randomAddress2), alcbMinted);
+        assertEq(alcb.getPoolBalance(), etherToSend / marketSpread);
+        assertEq(alcb.totalSupply(), totalSupplyBefore + alcbMinted);
+    }
+
+    function testMintToBigEthAmount() public {
+        uint256 totalSupplyBefore = alcb.totalSupply();
+        uint256 etherToSend = 70000000000 ether;
+        // fund the address
+        vm.deal(randomAddress, etherToSend);
+        // mock the call to use the address for the next call
+        vm.prank(randomAddress);
+
+        // call the mint function
+        uint256 alcbMinted = alcb.mintTo{value: etherToSend}(randomAddress2, 0);
+
+        // check the amount of alcb minted
+        assertEq(70001004975246203818081563855, alcbMinted);
+        assertEq(alcb.balanceOf(randomAddress2), alcbMinted);
+        assertEq(alcb.getPoolBalance(), etherToSend / marketSpread);
+        assertEq(alcb.totalSupply(), totalSupplyBefore + alcbMinted);
+    }
+
+    function testMintAlcbWithMinValueNotMetReverts() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(UtilityTokenErrors.MinimumValueNotMet.selector, 0, marketSpread)
+        );
+        // mock the call to use the address for the next call
+        vm.prank(randomAddress);
+
+        alcb.mint{value: 0 ether}(0);
+    }
+
+    function testMintAlcbToZeroAddressReverts() public {
+        // fund the address
+        vm.deal(randomAddress, 4 ether);
+        // mock the call to use the address for the next call
+        vm.prank(randomAddress);
+
+        vm.expectRevert(bytes("ERC20: mint to the zero address"));
+
+        alcb.mintTo{value: 4 ether}(zeroAddress, 0);
+    }
+}
