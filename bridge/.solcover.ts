5194ac0f5397cf8baa2bc3c3ce44074ec762f70a
Paul Bains <paul.bains@madhive.com>
2022-12-07 23:10:02 +0000

    Resolve code coverage timeout

--- bridge/.solcover.ts
+++ bridge/.solcover.ts
@@ -1,13 +1,13 @@
 module.exports = {
   configureYulOptimizer: true,
-  //   solcOptimizerDetails: {
-  //     peephole: false,
-  //     inliner: false,
-  //     jumpdestRemover: false,
-  //     orderLiterals: true, // <-- TRUE! Stack too deep when false
-  //     deduplicate: false,
-  //     cse: false,
-  //     constantOptimizer: false,
-  //     yul: true,
-  //   },
+  solcOptimizerDetails: {
+    peephole: false,
+    inliner: false,
+    jumpdestRemover: false,
+    orderLiterals: true, // <-- TRUE! Stack too deep when false
+    deduplicate: false,
+    cse: false,
+    constantOptimizer: false,
+    yul: true,
+  },
 };
