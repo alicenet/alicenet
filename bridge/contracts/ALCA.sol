a02c98fb9c4ef77bc9d918840499d14056c6c97b
ZJ <zijunlin32@gmail.com>
2022-12-07 12:44:47 -0500

    formatting

--- bridge/contracts/ALCA.sol
+++ bridge/contracts/ALCA.sol
@@ -20,7 +20,9 @@ contract ALCA is IStakingToken, ERC20, ImmutableFactory, ImmutableALCAMinter, Im
     address internal immutable _legacyToken;
     bool internal _hasEarlyStageEnded;
 
-    constructor(address legacyToken_)
+    constructor(
+        address legacyToken_
+    )
         ERC20("AliceNet Staking Token", "ALCA")
         ImmutableFactory(msg.sender)
         ImmutableALCAMinter()
