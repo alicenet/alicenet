50191363392dbd904a45b46f14e51b9d8378ed3b
Paul Bains <paul.bains@madhive.com>
2022-12-07 15:45:30 +0000

    Test cleanup

--- utils/dbtools_test.go
+++ utils/dbtools_test.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"os"
 	"testing"
 
 	"github.com/dgraph-io/badger/v2"
@@ -56,16 +55,7 @@ func TestDBValue(t *testing.T) {
 }
 
 func TestDBInt64(t *testing.T) {
-	dir, err := os.MkdirTemp("", "dbtools-test")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := os.RemoveAll(dir); err != nil {
-			t.Fatal(err)
-		}
-	}()
-	opts := badger.DefaultOptions(dir)
+	opts := badger.DefaultOptions(t.TempDir())
 	db, err := badger.Open(opts)
 	if err != nil {
 		t.Fatal(err)
