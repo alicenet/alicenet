bf747b249ad664afe929eb9fc84cca7d9894068f
Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>
2023-01-31 11:25:15 -0300

    Fix coverage run

--- bridge/.solcover.ts
+++ bridge/.solcover.ts
@@ -1,5 +1,19 @@
 module.exports = {
   configureYulOptimizer: true,
+  skipFiles: [
+    "test",
+    "mocks",
+    "node_modules",
+    "test_sol",
+    "lib",
+    "./contracts/BonusPool.sol",
+    "./contracts/Lockup.sol",
+    "./contracts/RewardPool.sol",
+  ],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
   //   solcOptimizerDetails: {
   //     peephole: false,
   //     inliner: false,
