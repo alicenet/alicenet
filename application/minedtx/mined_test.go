554b9c9bf35e4646a59321699a2b36a4ea62c7bd
Chris Gorman <chgorman@users.noreply.github.com>
2023-02-02 13:15:50 -0700

    Updated test functions and tests.

--- application/minedtx/mined_test.go
+++ application/minedtx/mined_test.go
@@ -94,47 +94,7 @@ func makeVSTXIn(t *testing.T, ownerSigner objs.Signer, txHash []byte) (*objs.TXO
 	return vs, txIn
 }
 
-func makeTxInitial(t *testing.T, ownerSigner objs.Signer) (objs.Vout, *objs.Tx) {
-	t.Helper()
-	consumedUTXOs := objs.Vout{}
-	txInputs := []*objs.TXIn{}
-	for i := 0; i < 2; i++ {
-		utxo, txin := makeVSTXIn(t, ownerSigner, nil)
-		consumedUTXOs = append(consumedUTXOs, utxo)
-		txInputs = append(txInputs, txin)
-	}
-	generatedUTXOs := objs.Vout{}
-	for i := 0; i < 2; i++ {
-		generatedUTXOs = append(generatedUTXOs, makeVS(t, ownerSigner))
-	}
-	err := generatedUTXOs.SetTxOutIdx()
-	if err != nil {
-		t.Fatal(err)
-	}
-	txfee := uint256.Zero()
-	tx := &objs.Tx{
-		Vin:  txInputs,
-		Vout: generatedUTXOs,
-		Fee:  txfee,
-	}
-	err = tx.SetTxHash()
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < 2; i++ {
-		vs, err := consumedUTXOs[i].ValueStore()
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = vs.Sign(tx.Vin[i], ownerSigner)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	return consumedUTXOs, tx
-}
-
-func makeTxInitial2(t *testing.T, ownerSigner objs.Signer, numTxIns uint32) (objs.Vout, *objs.Tx) {
+func makeTxInitial(t *testing.T, ownerSigner objs.Signer, numTxIns uint32) (objs.Vout, *objs.Tx) {
 	t.Helper()
 	if numTxIns == 0 {
 		t.Fatal("invalid numTxIns")
@@ -177,47 +137,6 @@ func makeTxInitial2(t *testing.T, ownerSigner objs.Signer, numTxIns uint32) (obj
 	return consumedUTXOs, tx
 }
 
-func makeTxConsuming(t *testing.T, ownerSigner objs.Signer, consumedUTXOs objs.Vout) *objs.Tx {
-	t.Helper()
-	txInputs := []*objs.TXIn{}
-	for i := 0; i < 2; i++ {
-		txin, err := consumedUTXOs[i].MakeTxIn()
-		if err != nil {
-			t.Fatal(err)
-		}
-		txInputs = append(txInputs, txin)
-	}
-	generatedUTXOs := objs.Vout{}
-	for i := 0; i < 2; i++ {
-		generatedUTXOs = append(generatedUTXOs, makeVS(t, ownerSigner))
-	}
-	err := generatedUTXOs.SetTxOutIdx()
-	if err != nil {
-		t.Fatal(err)
-	}
-	txfee := uint256.Zero()
-	tx := &objs.Tx{
-		Vin:  txInputs,
-		Vout: generatedUTXOs,
-		Fee:  txfee,
-	}
-	err = tx.SetTxHash()
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < 2; i++ {
-		vs, err := consumedUTXOs[i].ValueStore()
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = vs.Sign(tx.Vin[i], ownerSigner)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	return tx
-}
-
 func TestMined(t *testing.T) {
 	t.Parallel()
 	db := environment.SetupBadgerDatabase(t)
@@ -232,107 +151,8 @@ func TestMined(t *testing.T) {
 	storage := makeWrapperStorageMock()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
-
-	tx2 := makeTxConsuming(t, ownerSigner, consumedUTXOs)
-
-	_, err = tx.Validate(nil, 1, consumedUTXOs, storage)
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = tx2.Validate(nil, 1, tx.Vout, storage)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = db.Update(func(txn *badger.Txn) error {
-		txHash, err := tx.TxHash()
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = hndlr.Add(txn, 1, []*objs.Tx{tx})
-		if err != nil {
-			t.Fatal(err)
-		}
-		getTx1, _, err := hndlr.Get(txn, [][]byte{txHash})
-		if err != nil {
-			t.Fatal(err)
-		}
-		getTxHash1, err := getTx1[0].TxHash()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !bytes.Equal(getTxHash1, txHash) {
-			t.Fatalf("txHash mismatch:\noriginalHash:%x\nreturnedHash:%x\n", txHash, getTxHash1)
-		}
-		err = hndlr.Delete(txn, [][]byte{txHash})
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, missing, err := hndlr.Get(txn, [][]byte{txHash})
-		if len(missing) != 1 {
-			t.Error(err)
-			t.Fatal("delete failure")
-		}
-		tx2Hash, err := tx2.TxHash()
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = hndlr.Add(txn, 1, []*objs.Tx{tx, tx2})
-		if err != nil {
-			t.Fatal(err)
-		}
-		getTx1, _, err = hndlr.Get(txn, [][]byte{txHash})
-		if err != nil {
-			t.Fatal(err)
-		}
-		getTxHash1, err = getTx1[0].TxHash()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !bytes.Equal(getTxHash1, txHash) {
-			t.Fatalf("txHash mismatch:\noriginalHash:%x\nreturnedHash:%x\n", txHash, getTxHash1)
-		}
-		getTx2, _, err := hndlr.Get(txn, [][]byte{tx2Hash})
-		if err != nil {
-			t.Fatal(err)
-		}
-		getTxHash2, err := getTx2[0].TxHash()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !bytes.Equal(getTxHash2, tx2Hash) {
-			t.Fatalf("txHash mismatch:\noriginalHash:%x\nreturnedHash:%x\n", tx2Hash, getTxHash2)
-		}
-		atHeight, err := hndlr.GetHeightForTx(txn, getTxHash2)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if atHeight != 1 {
-			t.Fatalf("not at height 1: %v", atHeight)
-		}
-		return nil
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestMined2(t *testing.T) {
-	t.Parallel()
-	db := environment.SetupBadgerDatabase(t)
-	hndlr := NewMinedTxHandler()
-
-	signer := &crypto.BNSigner{}
-	err := signer.SetPrivk([]byte("secret"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	storage := makeWrapperStorageMock()
-
-	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial2(t, ownerSigner, 2)
-	consumedUTXOs2, tx2 := makeTxInitial2(t, ownerSigner, 3)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, 2)
+	consumedUTXOs2, tx2 := makeTxInitial(t, ownerSigner, 3)
 
 	_, err = tx.Validate(nil, 1, consumedUTXOs, storage)
 	if err != nil {
@@ -452,7 +272,8 @@ func TestMinedDelete(t *testing.T) {
 	hndlr := NewMinedTxHandler()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
+	numTxIns := uint32(3)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, numTxIns)
 
 	storage := makeWrapperStorageMock()
 
@@ -500,7 +321,8 @@ func TestMinedGet(t *testing.T) {
 	hndlr := NewMinedTxHandler()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
+	numTxIns := uint32(4)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, numTxIns)
 
 	storage := makeWrapperStorageMock()
 
@@ -568,7 +390,8 @@ func TestMinedGetHeightForTx(t *testing.T) {
 	hndlr := NewMinedTxHandler()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
+	numTxIns := uint32(3)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, numTxIns)
 
 	storage := makeWrapperStorageMock()
 
@@ -620,7 +443,8 @@ func TestMinedGetOneInternal(t *testing.T) {
 	hndlr := NewMinedTxHandler()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
+	numTxIns := uint32(3)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, numTxIns)
 
 	storage := makeWrapperStorageMock()
 
@@ -681,7 +505,8 @@ func TestMinedAddOneInternal(t *testing.T) {
 	hndlr := NewMinedTxHandler()
 
 	ownerSigner := testingOwner(t)
-	consumedUTXOs, tx := makeTxInitial(t, ownerSigner)
+	numTxIns := uint32(2)
+	consumedUTXOs, tx := makeTxInitial(t, ownerSigner, numTxIns)
 
 	storage := makeWrapperStorageMock()
 
