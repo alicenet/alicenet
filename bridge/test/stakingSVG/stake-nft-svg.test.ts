17b9c93ebf2e47365de53f2ccd3dbcb1203de321
zjlin <zijunlin32@gmail.com>
2023-02-14 12:33:32 -0500

    Update staking metadata svg (#667)
    ---------
    
    Co-authored-by: Leonardo Viana Teixeira <leo.mec.eng18@gmail.com>

--- bridge/test/stakingSVG/stake-nft-svg.test.ts
+++ bridge/test/stakingSVG/stake-nft-svg.test.ts
@@ -2,6 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
 import { StakingSVGMock } from "../../typechain-types";
 import { expect } from "../chai-setup";
+import { getStakingSVG } from "../setup";
 
 describe("StakingSVG: Tests StakingSVG library methods", async () => {
   let stakingNFTSVG: StakingSVGMock;
@@ -25,9 +26,12 @@ describe("StakingSVG: Tests StakingSVG library methods", async () => {
       accumulatorEth: "accumulatorEth value",
       accumulatorToken: "accumulatorToken value",
     };
-    const expectedSvgData = `<svg width="500" height="500" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink='http://www.w3.org/1999/xlink'><text x='10' y='20'>Shares: ${inputData.shares}</text><text x='10' y='40'>Free after: ${inputData.freeAfter}</text><text x='10' y='60'>Withdraw Free After: ${inputData.withdrawFreeAfter}</text><text x='10' y='80'>Accumulator (ETH): ${inputData.accumulatorEth}</text><text x='10' y='100'>Accumulator (Token): ${inputData.accumulatorToken}</text></svg>`;
+    const expectedSvgData = getStakingSVG(
+      inputData.shares,
+      inputData.freeAfter,
+      inputData.withdrawFreeAfter
+    );
     const generatedSVG = await stakingNFTSVG.generateSVG(inputData);
-
     await expect(generatedSVG).to.be.equal(expectedSvgData);
   });
 });
